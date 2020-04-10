238c618b065c75b2831e37e0e595635cd7576c80
Justin Britt <britt@uwindsor.ca>
2020-04-10 03:57:40 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Variables/RecoveredIndividuals/R.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Variables.RecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Oslo
+    using Microsoft.Research.Oslo;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables.RecoveredIndividuals;
+
+    internal sealed class R : IR
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R(
+            Vector value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public Vector Value { get; }
+    }
+}
---
+++ C19M.M.C.Gumel2004/Interfaces/Variables/RecoveredIndividuals/IR.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Variables.RecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Oslo
+    using Microsoft.Research.Oslo;
+
+    internal interface IR
+    {
+        Vector Value { get; }
+    }
+}
