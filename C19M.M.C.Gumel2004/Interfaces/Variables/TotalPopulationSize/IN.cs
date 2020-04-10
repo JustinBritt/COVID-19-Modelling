3bbffe589f11369816ab5bd85df9b6c0ffb6fb96
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:25:41 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Variables/TotalPopulationSize/IN.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Variables.TotalPopulationSize
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IN
+    {
+    }
+}
