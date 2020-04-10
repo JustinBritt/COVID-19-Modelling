85db94ddb7ce7c4060438d5c49763a6a6207495e
Justin Britt <britt@uwindsor.ca>
2020-04-09 23:27:46 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/PopulationDiminishmentRateAsymptomaticIndividualsToQuarantinedIndividuals/Iγ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.PopulationDiminishmentRateAsymptomaticIndividualsToQuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iγ
+    {
+        /// <summary>
+        /// Gets the population diminishment rate (symptomatic individuals to quarantined individuals).
+        /// </summary>
+        double Value { get; }
+    }
+}
