2a9fe9c95d173722544c70eb25444bba5b2a7290
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:21:43 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Variables/IsolatedIndividuals/J.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Variables.IsolatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables.IsolatedIndividuals;
+
+    internal sealed class J : IJ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public J()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
