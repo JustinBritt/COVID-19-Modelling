ea1acbefbf0f418f4a16c919cd62e86fcb5d17be
Justin Britt <britt@uwindsor.ca>
2020-04-09 23:50:58 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Parameters/IsolationRateSymptomaticIndividuals/γ.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Parameters.IsolationRateSymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals;
+
+    internal sealed class γ : Iγ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public γ(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the isolation rate for symptomatic individuals.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/IsolationRateSymptomaticIndividuals/Iγ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iγ
+    {
+        /// <summary>
+        /// Gets the isolation rate for symptomatic individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
