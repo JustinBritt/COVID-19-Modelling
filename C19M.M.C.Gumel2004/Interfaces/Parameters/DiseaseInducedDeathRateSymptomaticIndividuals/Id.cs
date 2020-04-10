95e823d845cbf656f03152af3a9e547138d51dfc
Justin Britt <britt@uwindsor.ca>
2020-04-09 23:54:16 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/DiseaseInducedDeathRateSymptomaticIndividuals/Id.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.DiseaseInducedDeathRateSymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Id
+    {
+        /// <summary>
+        /// Gets the disease-induced death rate for symptomatic individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
