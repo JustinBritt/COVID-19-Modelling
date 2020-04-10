113a1a686bad2700cc133e58d42744b9140f74ea
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:14:36 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Variables/AsymptomaticIndividuals/E.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Variables.AsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables.AsymptomaticIndividuals;
+
+    internal sealed class E : IE
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
