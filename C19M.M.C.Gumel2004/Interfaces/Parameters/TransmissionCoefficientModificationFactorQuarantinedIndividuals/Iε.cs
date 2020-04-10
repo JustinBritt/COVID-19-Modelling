af546fc3430e99e27ed90a82bd9c704fefed1457
Justin Britt <britt@uwindsor.ca>
2020-04-09 23:01:30 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/TransmissionCoefficientModificationFactorQuarantinedIndividuals/Iε.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorQuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iε
+    {
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for quarantined individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
