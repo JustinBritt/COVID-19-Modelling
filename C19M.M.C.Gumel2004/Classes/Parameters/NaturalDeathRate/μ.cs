e4eaa16d7bce827e11340447286c8731055605e5
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:32:13 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Parameters/NaturalDeathRate/μ.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Parameters.NaturalDeathRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
+    internal sealed class μ : Iμ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public μ(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the positive Value (i.e., μ > 0) of the natural death rate.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/NaturalDeathRate/Iμ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iμ
+    {
+        /// <summary>
+        /// Gets the positive Value (i.e., μ > 0) of the natural death rate.
+        /// </summary>
+        double Value { get; }
+    }
+}
