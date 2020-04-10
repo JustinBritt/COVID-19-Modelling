bd2d798c1360279196010e9eaa132dd2be95bbc7
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:47:44 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/DailyRecruitmentRateAsymptomaticIndividuals/Ip.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.RecruitmentRateAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Ip
+    {
+        /// <summary>
+        /// Gets the recruitment rate of asymptomatic individuals per day.
+        /// </summary>
+        double Value { get; }
+    }
+}
