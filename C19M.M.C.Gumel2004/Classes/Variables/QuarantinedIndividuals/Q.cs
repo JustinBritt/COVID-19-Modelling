6dad5b6afd838b563a6dc8c899ecb106c7d5e644
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:17:10 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Variables/QuarantinedIndividuals/Q.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Variables.QuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables.QuarantinedIndividuals;
+
+    internal sealed class Q : IQ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Q()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
