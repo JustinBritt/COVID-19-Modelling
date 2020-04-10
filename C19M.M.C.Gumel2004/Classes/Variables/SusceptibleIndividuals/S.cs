cf8c6ebd6fc2cf38ed729cedef4442870ae6cbfc
Justin Britt <britt@uwindsor.ca>
2020-04-09 20:11:35 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Variables/SusceptibleIndividuals/S.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Variables.SusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables.SusceptibleIndividuals;
+
+    internal sealed class S : IS
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
