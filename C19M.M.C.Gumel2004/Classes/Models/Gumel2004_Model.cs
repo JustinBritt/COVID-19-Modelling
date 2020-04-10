6300e4f58385e7d875ce79fe5f7b487cdc19c54c
Justin Britt <britt@uwindsor.ca>
2020-04-10 00:10:47 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Models/Gumel2004_Model.cs
@@ -0,0 +1,25 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Models;
+
+    internal sealed class Gumel2004_Model : IGumel2004_Model
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Gumel2004_Model(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
