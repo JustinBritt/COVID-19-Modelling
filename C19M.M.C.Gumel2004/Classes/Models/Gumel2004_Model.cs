4cad4d6d55e9341a1d429ae68dfcc7a27d60167f
Justin Britt <britt@uwindsor.ca>
2020-04-10 03:49:52 -0400

    Changes to usings

--- C19M.M.C.Gumel2004/Classes/Models/Gumel2004_Model.cs
+++ C19M.M.C.Gumel2004/Classes/Models/Gumel2004_Model.cs
@@ -14,6 +14,26 @@
 
     using C19M.M.C.Gumel2004.Interfaces.Models;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.BasicReproductionNumber;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.BasicTransmissionCoefficient;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.ControlReproductionNumber;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateQuarantinedIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.DiseaseInducedDeathRateIsolatedIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.DiseaseInducedDeathRateSymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NetInflowRateSusceptibleIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.QuarantineRateAsymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.RecoveryRateIsolatedIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.RecoveryRateSymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.RecruitmentRateAsymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorAsymptomaticIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorIsolatedIndividuals;
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorQuarantinedIndividuals;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables;
+
     internal sealed class Gumel2004_Model : IGumel2004_Model
     {
         private ILog Log { get; }
