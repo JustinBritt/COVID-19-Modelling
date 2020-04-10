b73c7a97a7fd9c54c00780880ab85477b9be3948
Justin Britt <britt@uwindsor.ca>
2020-04-10 01:26:07 -0400

    Changes to usings (Oslo)

--- C19M.M.C.Gumel2004/Classes/Variables/AsymptomaticIndividuals/E.cs
+++ C19M.M.C.Gumel2004/Classes/Variables/AsymptomaticIndividuals/E.cs
@@ -9,6 +9,9 @@
     // Logging
     using log4net;
 
+    // Oslo
+    using Microsoft.Research.Oslo;
+
     using C19M.M.C.Gumel2004.Interfaces.Variables.AsymptomaticIndividuals;
 
     internal sealed class E : IE
