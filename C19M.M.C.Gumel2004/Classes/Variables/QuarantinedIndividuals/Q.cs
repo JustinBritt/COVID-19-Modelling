26a6d271b11cfddd2d25d6189876036fe984be4c
Justin Britt <britt@uwindsor.ca>
2020-04-11 18:58:43 -0400

    Added method to get dQ/dt

--- C19M.M.C.Gumel2004/Classes/Variables/QuarantinedIndividuals/Q.cs
+++ C19M.M.C.Gumel2004/Classes/Variables/QuarantinedIndividuals/Q.cs
@@ -12,6 +12,8 @@
     // Oslo
     using Microsoft.Research.Oslo;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
     using C19M.M.C.Gumel2004.Interfaces.Variables.QuarantinedIndividuals;
 
     internal sealed class Q : IQ
@@ -28,5 +30,27 @@
         }
 
         public Vector Value { get; }
+
+        /// <summary>
+        /// Gets dQ/dt.
+        /// </summary>
+        /// <param name="E"></param>
+        /// <param name="Q"></param>
+        /// <param name="γ_1"></param>
+        /// <param name="κ_2"></param>
+        /// <param name="μ"></param>
+        /// <returns></returns>
+        public double GetdQdt(
+            double E,
+            double Q,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.QuarantineRateAsymptomaticIndividuals.Iγ γ_1,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateQuarantinedIndividuals.Iκ κ_2,
+            Iμ μ)
+        {
+            return
+                γ_1.Value * E
+                -
+                (κ_2.Value + μ.Value) * Q;
+        }
     }
 }
--- C19M.M.C.Gumel2004/Interfaces/Variables/QuarantinedIndividuals/IQ.cs
+++ C19M.M.C.Gumel2004/Interfaces/Variables/QuarantinedIndividuals/IQ.cs
@@ -9,8 +9,26 @@
     // Oslo
     using Microsoft.Research.Oslo;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
     internal interface IQ
     {
         Vector Value { get; }
+
+        /// <summary>
+        /// Gets dQ/dt.
+        /// </summary>
+        /// <param name="E"></param>
+        /// <param name="Q"></param>
+        /// <param name="γ_1"></param>
+        /// <param name="κ_2"></param>
+        /// <param name="μ"></param>
+        /// <returns></returns>
+        double GetdQdt(
+            double E,
+            double Q,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.QuarantineRateAsymptomaticIndividuals.Iγ γ_1,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateQuarantinedIndividuals.Iκ κ_2,
+            Iμ μ);
     }
 }
