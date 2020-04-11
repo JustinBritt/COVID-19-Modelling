0b40b2e084efb456a68f149bafc91c1453432b92
Justin Britt <britt@uwindsor.ca>
2020-04-11 12:19:30 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/InitialValueRecoveredIndividuals/IR.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.InitialValueRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IR
+    {
+        /// <summary>
+        /// Gets the initial value for recovered individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
