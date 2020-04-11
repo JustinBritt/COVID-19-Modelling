5c097d2a6ee5a830ad86851571e35183717946ce
Justin Britt <britt@uwindsor.ca>
2020-04-10 21:28:47 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Parameters/InitialValueSymptomaticIndividuals/I.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Parameters.InitialValueSymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.InitialValueSymptomaticIndividuals;
+
+    internal sealed class I : II
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public I(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the initial value for symptomatic individuals.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/InitialValueSymptomaticIndividuals/II.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.InitialValueSymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface II
+    {
+        /// <summary>
+        /// Gets the initial value for symptomatic individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
