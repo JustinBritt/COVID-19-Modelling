f4eaf2c97562ed879675dd9ea446b709e5a3c87e
Justin Britt <britt@uwindsor.ca>
2020-04-11 18:38:51 -0400

    Added method to get dJ/dt

--- C19M.M.C.Gumel2004/Interfaces/Variables/IsolatedIndividuals/IJ.cs
+++ C19M.M.C.Gumel2004/Interfaces/Variables/IsolatedIndividuals/IJ.cs
@@ -9,8 +9,34 @@
     // Oslo
     using Microsoft.Research.Oslo;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
+    using C19M.M.C.Gumel2004.Interfaces.Variables.IsolatedIndividuals;
+
     internal interface IJ
     {
         Vector Value { get; }
+
+        /// <summary>
+        /// Gets dJ/dt.
+        /// </summary>
+        /// <param name="d_2"></param>
+        /// <param name="I"></param>
+        /// <param name="J"></param>
+        /// <param name="Q"></param>
+        /// <param name="γ_2"></param>
+        /// <param name="κ_2"></param>
+        /// <param name="μ"></param>
+        /// <param name="σ_2"></param>
+        /// <returns></returns>
+        double GetdJdt(
+            Interfaces.Parameters.DiseaseInducedDeathRateIsolatedIndividuals.Id d_2,
+            double I,
+            double J,
+            double Q,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals.Iγ γ_2,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateQuarantinedIndividuals.Iκ κ_2,
+            Iμ μ,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.RecoveryRateIsolatedIndividuals.Iσ σ_2);
     }
 }
