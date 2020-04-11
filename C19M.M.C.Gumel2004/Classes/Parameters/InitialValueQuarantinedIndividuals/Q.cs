1f8f32a6ed478d76786dadb03602ecf0056561a3
Justin Britt <britt@uwindsor.ca>
2020-04-11 07:56:18 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Parameters/InitialValueQuarantinedIndividuals/Q.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Parameters.InitialValueQuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.InitialValueQuarantinedIndividuals;
+
+    internal sealed class Q : IQ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Q(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the initial value for quarantined individuals.
+        /// </summary>
+        public double Value { get; }
+    }
+}
