69ef0a896c6331bd7f4394d50c5bee1dcfc8725c
Justin Britt <britt@uwindsor.ca>
2020-04-10 22:16:01 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/InitialValueIsolatedIndividuals/IJ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.InitialValueIsolatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IJ
+    {
+        /// <summary>
+        /// Gets the initial value for isolated individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
