320b64bbd852ec2e08f8adf8aff27aee1bb949f1
Justin Britt <britt@uwindsor.ca>
2020-04-10 20:46:46 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Interfaces/Parameters/InitialValueAsymptomaticIndividuals/IE.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.Gumel2004.Interfaces.Parameters.InitialValueAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IE
+    {
+        /// <summary>
+        /// Gets the initial value for asymptomatic individuals.
+        /// </summary>
+        double Value { get; }
+    }
+}
