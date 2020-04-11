6137a81126f2fdcf2ab914d9532309202db913f3
Justin Britt <britt@uwindsor.ca>
2020-04-11 18:46:51 -0400

    Added method to get dI/dt

--- C19M.M.C.Gumel2004/Classes/Variables/SymptomaticIndividuals/I.cs
+++ C19M.M.C.Gumel2004/Classes/Variables/SymptomaticIndividuals/I.cs
@@ -12,6 +12,8 @@
     // Oslo
     using Microsoft.Research.Oslo;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
     using C19M.M.C.Gumel2004.Interfaces.Variables.SymptomaticIndividuals;
 
     internal sealed class I : II
@@ -28,5 +30,31 @@
         }
 
         public Vector Value { get; }
+
+        /// <summary>
+        /// Gets dI/dt.
+        /// </summary>
+        /// <param name="d_1"></param>
+        /// <param name="E"></param>
+        /// <param name="I"></param>
+        /// <param name="γ_2"></param>
+        /// <param name="κ_1"></param>
+        /// <param name="μ"></param>
+        /// <param name="σ_1"></param>
+        /// <returns></returns>
+        public double GetdIdt(
+            Interfaces.Parameters.DiseaseInducedDeathRateSymptomaticIndividuals.Id d_1,
+            double E,
+            double I,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals.Iγ γ_2,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals.Iκ κ_1,
+            Iμ μ,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.RecoveryRateSymptomaticIndividuals.Iσ σ_1)
+        {
+            return
+                κ_1.Value * E
+                -
+                (γ_2.Value + d_1.Value + σ_1.Value + μ.Value) * I;
+        }
     }
 }
--- C19M.M.C.Gumel2004/Interfaces/Variables/SymptomaticIndividuals/II.cs
+++ C19M.M.C.Gumel2004/Interfaces/Variables/SymptomaticIndividuals/II.cs
@@ -9,8 +9,30 @@
     // Oslo
     using Microsoft.Research.Oslo;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
     internal interface II
     {
         Vector Value { get; }
+
+        /// <summary>
+        /// Gets dI/dt.
+        /// </summary>
+        /// <param name="d_1"></param>
+        /// <param name="E"></param>
+        /// <param name="I"></param>
+        /// <param name="γ_2"></param>
+        /// <param name="κ_1"></param>
+        /// <param name="μ"></param>
+        /// <param name="σ_1"></param>
+        /// <returns></returns>
+        double GetdIdt(
+            Interfaces.Parameters.DiseaseInducedDeathRateSymptomaticIndividuals.Id d_1,
+            double E,
+            double I,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals.Iγ γ_2,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals.Iκ κ_1,
+            Iμ μ,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.RecoveryRateSymptomaticIndividuals.Iσ σ_1);
     }
 }
