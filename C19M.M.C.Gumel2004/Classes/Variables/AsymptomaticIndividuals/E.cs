6cf8819841ee21c263ecb87648985f50eccb6f6e
Justin Britt <britt@uwindsor.ca>
2020-04-11 18:30:26 -0400

    Added method to get dE/dt

--- C19M.M.C.Gumel2004/Classes/Variables/AsymptomaticIndividuals/E.cs
+++ C19M.M.C.Gumel2004/Classes/Variables/AsymptomaticIndividuals/E.cs
@@ -12,6 +12,12 @@
     // Oslo
     using Microsoft.Research.Oslo;
 
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.BasicTransmissionCoefficient;
+
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.NaturalDeathRate;
+
+    using C19M.M.C.Gumel2004.Interfaces.Parameters.RecruitmentRateAsymptomaticIndividuals;
+
     using C19M.M.C.Gumel2004.Interfaces.Variables.AsymptomaticIndividuals;
 
     internal sealed class E : IE
@@ -28,5 +34,49 @@
         }
 
         public Vector Value { get; }
+
+        /// <summary>
+        /// Gets dE/dt.
+        /// </summary>
+        /// <param name="E"></param>
+        /// <param name="I"></param>
+        /// <param name="J"></param>
+        /// <param name="N"></param>
+        /// <param name="p"></param>
+        /// <param name="Q"></param>
+        /// <param name="S"></param>
+        /// <param name="β"></param>
+        /// <param name="γ_1"></param>
+        /// <param name="γ_2"></param>
+        /// <param name="ε_E"></param>
+        /// <param name="ε_J"></param>
+        /// <param name="ε_Q"></param>
+        /// <param name="κ_1"></param>
+        /// <param name="μ"></param>
+        /// <returns></returns>
+        public double GetdEdt(
+            double E,
+            double I,
+            double J,
+            double N,
+            Ip p,
+            double Q,
+            double S,
+            Iβ β,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.QuarantineRateAsymptomaticIndividuals.Iγ γ_1,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals.Iγ γ_2,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorAsymptomaticIndividuals.Iε ε_E,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorIsolatedIndividuals.Iε ε_J,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorQuarantinedIndividuals.Iε ε_Q,
+            C19M.M.C.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals.Iκ κ_1,
+            Iμ μ)
+        {
+            return 
+                p.Value
+                +
+                (S / N) * (β.Value) * (I + ε_E.Value * E + ε_Q.Value * Q + ε_J.Value * J)
+                -
+                (γ_1.Value + κ_1.Value + μ.Value) * E;
+        }
     }
 }
