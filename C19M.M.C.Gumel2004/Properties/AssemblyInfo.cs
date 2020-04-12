c7e5520729531eb8b16c7e7c72c72b301e27db49
Justin Britt <britt@uwindsor.ca>
2020-04-12 05:37:53 -0400

    Allow for unit testing

--- C19M.M.C.Gumel2004/Properties/AssemblyInfo.cs
+++ C19M.M.C.Gumel2004/Properties/AssemblyInfo.cs
@@ -22,6 +22,8 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4138762d-a11a-4f0b-a51b-17bfb983ae87")]
 
+[assembly: InternalsVisibleTo("C19M.M.C.Gumel2004.Tests")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
