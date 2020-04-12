8f26a1868962e3fd11fc7aee6db212a5d1c8dfd9
Justin Britt <britt@uwindsor.ca>
2020-04-12 00:15:03 -0400

    Added class

---
+++ C19M.M.C.Gumel2004/Classes/Exports/Gumel2004_Export.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.Gumel2004.Classes.Exports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.Gumel2004.Interfaces.Exports;
+
+    public sealed class Gumel2004_Export : IGumel2004_Export
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Gumel2004_Export()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
