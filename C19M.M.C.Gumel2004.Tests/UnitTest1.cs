fd343da0a9810d72e07b96b0ecd1434165abb525
Justin Britt <britt@uwindsor.ca>
2020-04-12 05:36:14 -0400

    Added unit test project

---
+++ C19M.M.C.Gumel2004.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace C19M.M.C.Gumel2004.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
