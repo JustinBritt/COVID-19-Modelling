ed789b45f1cbe1a522d461d704a2511df0aa5966
Justin Britt <britt@uwindsor.ca>
2020-04-12 00:22:20 -0400

    Added class

---
+++ C19M.D.Gumel2004/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C19M.D.Gumel2004
+{
+    public class Class1
+    {
+    }
+}
