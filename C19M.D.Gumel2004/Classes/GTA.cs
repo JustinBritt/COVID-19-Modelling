2a881b1ef66f74c235ac255b884bfcad608a2f31
Justin Britt <britt@uwindsor.ca>
2020-04-12 00:42:34 -0400

    Added class

---
+++ C19M.D.Gumel2004/Classes/GTA.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.D.Gumel2004.Classes
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.D.Gumel2004.Interfaces;
+
+    /// <summary>
+    /// Represents data from the Greater Toronto Area (GTA).
+    /// </summary>
+    public sealed class GTA : IGTA
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public GTA()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.D.Gumel2004/Interfaces/IGTA.cs
@@ -0,0 +1,15 @@
+﻿namespace C19M.D.Gumel2004.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents data from the Greater Toronto Area (GTA).
+    /// </summary>
+    public interface IGTA
+    {
+    }
+}
