3ac8f3641deaabede35a44b12637ef5cb896a2c5
Justin Britt <britt@uwindsor.ca>
2020-04-12 05:33:08 -0400

    Added data

--- C19M.D.Gumel2004/Classes/GTA.cs
+++ C19M.D.Gumel2004/Classes/GTA.cs
@@ -23,5 +23,131 @@
         {
             this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         }
+
+        /// <summary>
+        /// Gets the disease-induced death rate for symptomatic individuals.
+        /// Parameter: d_1
+        /// </summary>
+        public double DiseaseInducedDeathRateSymptomaticIndividuals => 0.0079;
+
+        /// <summary>
+        /// Gets the disease-induced death rate for isolated individuals.
+        /// Parameter: d_2
+        /// </summary>
+        public double DiseaseInducedDeathRateIsolatedIndividuals => 0.0068;
+
+        /// <summary>
+        /// Gets the initial value for asymptomatic individuals.
+        /// Parameter: E(0)
+        /// </summary>
+        public double InitialValueAsymptomaticIndividuals => 6;
+
+        /// <summary>
+        /// Gets the initial value for symptomatic individuals.
+        /// Parameter: I(0)
+        /// </summary>
+        public double InitialValueSymptomaticIndividuals => 1;
+
+        /// <summary>
+        /// Gets the initial value for isolated individuals.
+        /// Parameter: J(0)
+        /// </summary>
+        public double InitialValueIsolatedIndividuals => 0;
+
+        /// <summary>
+        /// Gets the recruitment rate of asymptomatic individuals per day.
+        /// Parameter: p
+        /// </summary>
+        public double RecruitmentRateAsymptomaticIndividuals => 0.06;
+
+        /// <summary>
+        /// Gets the initial value for quarantined individuals.
+        /// Parameter: Q(0)
+        /// </summary>
+        public double InitialValueQuarantinedIndividuals => 0;
+
+        /// <summary>
+        /// Gets the initial value for recovered individuals.
+        /// Parameter: R(0)
+        /// </summary>
+        public double InitialValueRecoveredIndividuals => 0;
+
+        /// <summary>
+        /// Gets the initial value for susceptible individuals.
+        /// Parameter: S(0)
+        /// </summary>
+        public double InitialValueSusceptibleIndividuals => 4;
+
+        /// <summary>
+        /// Gets the basic transmission coefficient.
+        /// Parameter: β
+        /// </summary>
+        public double BasicTransmissionCoefficient => 0.2;
+
+        /// <summary>
+        /// Gets the quarantine rate for asymptomatic individuals.
+        /// Parameter: γ_1
+        /// </summary>
+        public double QuarantineRateAsymptomaticIndividuals => 0; // Untul 30 March 2003
+
+        /// <summary>
+        /// Gets the isolation rate for symptomatic individuals.
+        /// Parameter: γ_2
+        /// </summary>
+        public double IsolationRateSymptomaticIndividuals => 0; // Until 30 March 2003
+
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for asymptomatic individuals.
+        /// Parameter: ε_E
+        /// </summary>
+        public double TransmissionCoefficientModificationFactorAsymptomaticIndividuals => 0; // ?
+
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for isolated individuals.
+        /// Parameter: ε_J
+        /// </summary>
+        public double TransmissionCoefficientModificationFactorIsolatedIndividuals => 0.36;
+
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for quarantined individuals.
+        /// Parameter: ε_Q
+        /// </summary>
+        public double TransmissionCoefficientModificationFactorQuarantinedIndividuals => 0; // ?
+
+        /// <summary>
+        /// Gets the rate at which asymptomatic individuals develop clinical symptoms.
+        /// Parameter: κ_1
+        /// </summary>
+        public double DevelopmentClinicalSymptomsRateAsymptomaticIndividuals => 0.1;
+
+        /// <summary>
+        /// Gets the rate at which quarantined individuals develop clinical symptoms.
+        /// Parameter: κ_2
+        /// </summary>
+        public double DevelopmentClinicalSymptomsRateQuarantinedIndividuals => 0.125;
+
+        /// <summary>
+        /// Gets the natural death rate.
+        /// Parameter: μ
+        /// </summary>
+        public double NaturalDeathRate => 0.000034;
+
+        /// <summary>
+        /// Gets the net inflow rate of susceptible individuals per unit time.
+        /// Parameter: Π
+        /// </summary>
+        public double NetInflowRateSusceptibleIndividuals => 136;
+
+        /// <summary>
+        /// Gets the recovery rate for symptomatic individuals.
+        /// Parameter: σ_1
+        /// </summary>
+        public double RecoveryRateSymptomaticIndividuals => 0.0337;
+
+        /// <summary>
+        /// Gets the recovery rate for isolated individuals.
+        /// Parameter: σ_2
+        /// </summary>
+        public double RecoveryRateIsolatedIndividuals => 0.0386;
     }
 }
--- C19M.D.Gumel2004/Interfaces/IGTA.cs
+++ C19M.D.Gumel2004/Interfaces/IGTA.cs
@@ -11,5 +11,130 @@
     /// </summary>
     public interface IGTA
     {
+        /// <summary>
+        /// Gets the disease-induced death rate for symptomatic individuals.
+        /// Parameter: d_1
+        /// </summary>
+        double DiseaseInducedDeathRateSymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the disease-induced death rate for isolated individuals.
+        /// Parameter: d_2
+        /// </summary>
+        double DiseaseInducedDeathRateIsolatedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the initial value for asymptomatic individuals.
+        /// Parameter: E(0)
+        /// </summary>
+        double InitialValueAsymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the initial value for symptomatic individuals.
+        /// Parameter: I(0)
+        /// </summary>
+        double InitialValueSymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the initial value for isolated individuals.
+        /// Parameter: J(0)
+        /// </summary>
+        double InitialValueIsolatedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the recruitment rate of asymptomatic individuals per day.
+        /// Parameter: p
+        /// </summary>
+        double RecruitmentRateAsymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the initial value for quarantined individuals.
+        /// Parameter: Q(0)
+        /// </summary>
+        double InitialValueQuarantinedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the initial value for recovered individuals.
+        /// Parameter: R(0)
+        /// </summary>
+        double InitialValueRecoveredIndividuals { get; }
+
+        /// <summary>
+        /// Gets the initial value for susceptible individuals.
+        /// Parameter: S(0)
+        /// </summary>
+        double InitialValueSusceptibleIndividuals { get; }
+
+        /// <summary>
+        /// Gets the basic transmission coefficient.
+        /// Parameter: β
+        /// </summary>
+        double BasicTransmissionCoefficient { get; }
+
+        /// <summary>
+        /// Gets the quarantine rate for asymptomatic individuals.
+        /// Parameter: γ_1
+        /// </summary>
+        double QuarantineRateAsymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the isolation rate for symptomatic individuals.
+        /// Parameter: γ_2
+        /// </summary>
+        double IsolationRateSymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for asymptomatic individuals.
+        /// Parameter: ε_E
+        /// </summary>
+        double TransmissionCoefficientModificationFactorAsymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for isolated individuals.
+        /// Parameter: ε_J
+        /// </summary>
+        double TransmissionCoefficientModificationFactorIsolatedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the transmission coefficient modification factor for quarantined individuals.
+        /// Parameter: ε_Q
+        /// </summary>
+        double TransmissionCoefficientModificationFactorQuarantinedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the rate at which asymptomatic individuals develop clinical symptoms.
+        /// Parameter: κ_1
+        /// </summary>
+        double DevelopmentClinicalSymptomsRateAsymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the rate at which quarantined individuals develop clinical symptoms.
+        /// Parameter: κ_2
+        /// </summary>
+        double DevelopmentClinicalSymptomsRateQuarantinedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the natural death rate.
+        /// Parameter: μ
+        /// </summary>
+        double NaturalDeathRate { get; }
+
+        /// <summary>
+        /// Gets the net inflow rate of susceptible individuals per unit time.
+        /// Parameter: Π
+        /// </summary>
+        double NetInflowRateSusceptibleIndividuals { get; }
+
+        /// <summary>
+        /// Gets the recovery rate for symptomatic individuals.
+        /// Parameter: σ_1
+        /// </summary>
+        double RecoveryRateSymptomaticIndividuals { get; }
+
+        /// <summary>
+        /// Gets the recovery rate for isolated individuals.
+        /// Parameter: σ_2
+        /// </summary>
+        double RecoveryRateIsolatedIndividuals { get; }
     }
 }
