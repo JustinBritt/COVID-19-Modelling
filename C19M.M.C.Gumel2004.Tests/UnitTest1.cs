474007ce5ad6bf07c114a242f729733f6abdef81
Justin Britt <britt@uwindsor.ca>
2020-04-13 12:27:53 -0400

    Changes to outputs

--- C19M.M.C.Gumel2004.Tests/UnitTest1.cs
+++ C19M.M.C.Gumel2004.Tests/UnitTest1.cs
@@ -70,13 +70,15 @@
 
             double d_2 = 0.0068;
 
+            double deaths = 0;
+
             for (int i = 0; i <= results.Length - 1; i = i + 1)
             {
                 MathNet.Numerics.LinearAlgebra.Vector<double> w = results[i];
 
-                double deaths = d_1 * w[1] + d_2 * w[2];
+                deaths += d_1 * w[1] + d_2 * w[2];
 
-                System.Diagnostics.Debug.WriteLine(w[1]);
+                System.Diagnostics.Debug.WriteLine(deaths);
 
                 // S(t)
                 //System.Diagnostics.Debug.WriteLine(w[5]);
