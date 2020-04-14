dbb521711876e0497ebd3f95cd408c0c94f7f2ad
Justin Britt <britt@uwindsor.ca>
2020-04-14 11:21:33 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/DevelopmentClinicalSymptomsRateAsymptomaticIndividuals/α.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals;
+
+    internal sealed class α : Iα
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public α(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the rate of development of clinical symptoms.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/DevelopmentClinicalSymptomsRateAsymptomaticIndividuals/Iα.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iα
+    {
+        /// <summary>
+        /// Gets the rate of development of clinical symptoms.
+        /// </summary>
+        double Value { get; }
+    }
+}
