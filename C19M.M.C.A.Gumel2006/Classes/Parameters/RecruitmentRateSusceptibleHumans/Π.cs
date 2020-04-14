5cbf80d7ff5b89dcf6a9b39ef67b84adb42850ca
Justin Britt <britt@uwindsor.ca>
2020-04-14 11:39:46 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/RecruitmentRateSusceptibleHumans/Π.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.RecruitmentRateSusceptibleHumans
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecruitmentRateSusceptibleHumans;
+
+    internal sealed class Π : IΠ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Π(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the recruitment rate of susceptible humans.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/RecruitmentRateSusceptibleHumans/IΠ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecruitmentRateSusceptibleHumans
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IΠ
+    {
+        /// <summary>
+        /// Gets the recruitment rate of susceptible humans.
+        /// </summary>
+        double Value { get; }
+    }
+}
