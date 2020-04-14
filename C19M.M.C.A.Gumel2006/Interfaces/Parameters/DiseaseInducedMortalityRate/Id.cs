f29ab1235d0ac396063a78b8ba4f1273e702bc38
Justin Britt <britt@uwindsor.ca>
2020-04-14 11:13:10 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/DiseaseInducedMortalityRate/Id.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.DiseaseInducedMortalityRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Id
+    {
+        /// <summary>
+        /// Gets the disease-induced mortality rate.
+        /// </summary>
+        double Value { get; }
+    }
+}
