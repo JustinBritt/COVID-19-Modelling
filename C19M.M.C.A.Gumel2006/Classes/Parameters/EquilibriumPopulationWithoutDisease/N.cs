6b8784dea4f658526f7b14214328584b21c173f9
Justin Britt <britt@uwindsor.ca>
2020-04-14 11:18:19 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/EquilibriumPopulationWithoutDisease/N.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.EquilibriumPopulationWithoutDisease
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.EquilibriumPopulationWithoutDisease;
+
+    internal sealed class N : IN
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public N(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the equilibrium population (without disease).
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/EquilibriumPopulationWithoutDisease/IN.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.EquilibriumPopulationWithoutDisease
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IN
+    {
+        /// <summary>
+        /// Gets the equilibrium population (without disease).
+        /// </summary>
+        double Value { get; }
+    }
+}
