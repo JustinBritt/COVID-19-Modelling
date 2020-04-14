1b32ca01fd848c1c836f8863730eeb7c84bb7f50
Justin Britt <britt@uwindsor.ca>
2020-04-14 11:30:29 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/RecoveryRate/δ.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.RecoveryRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecoveryRate;
+
+    internal sealed class δ : Iδ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public δ(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the recovery rate.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/RecoveryRate/Iδ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecoveryRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iδ
+    {
+        /// <summary>
+        /// Gets the recovery rate.
+        /// </summary>
+        double Value { get; }
+    }
+}
