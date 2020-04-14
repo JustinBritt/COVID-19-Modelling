8d21cb077109dc663ababe7961ec3a06d2da8a40
Justin Britt <britt@uwindsor.ca>
2020-04-14 11:37:08 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/VaccinationCoverageRate/ξ.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.VaccinationCoverageRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.VaccinationCoverageRate;
+
+    internal sealed class ξ : Iξ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public ξ(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the vaccination coverage rate.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/VaccinationCoverageRate/Iξ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.VaccinationCoverageRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iξ
+    {
+        /// <summary>
+        /// Gets the vaccination coverage rate.
+        /// </summary>
+        double Value { get; }
+    }
+}
