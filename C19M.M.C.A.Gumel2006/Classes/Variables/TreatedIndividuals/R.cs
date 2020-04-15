00fec7257a8974c9515ff98f4d6da9e07f04698f
Justin Britt <britt@uwindsor.ca>
2020-04-15 10:55:07 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Variables/TreatedIndividuals/R.cs
@@ -0,0 +1,29 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Variables.TreatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.TreatedIndividuals;
+
+    internal sealed class R : IR
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R(
+            double initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public double InitialValue { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Variables/TreatedIndividuals/IR.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Variables.TreatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IR
+    {
+        double InitialValue { get; }
+    }
+}
