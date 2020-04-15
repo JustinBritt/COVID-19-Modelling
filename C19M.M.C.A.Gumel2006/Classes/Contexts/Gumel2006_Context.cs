00bdb20d0aa4c195ffa15fda9d90a7916d547ec0
Justin Britt <britt@uwindsor.ca>
2020-04-15 11:03:46 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Contexts/Gumel2006_Context.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Contexts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Contexts;
+
+    public sealed class Gumel2006_Context : IGumel2006_Context
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Gumel2006_Context()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Contexts/IGumel2006_Context.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Contexts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IGumel2006_Context
+    {
+    }
+}
