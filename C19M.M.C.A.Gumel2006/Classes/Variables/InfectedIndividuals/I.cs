6355c00230b7d81594963e65142afef5cf035704
Justin Britt <britt@uwindsor.ca>
2020-04-15 10:53:09 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Variables/InfectedIndividuals/I.cs
@@ -0,0 +1,29 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Variables.InfectedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.InfectedIndividuals;
+
+    internal sealed class I : II
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public I(
+            double initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public double InitialValue { get; }
+    }
+}
