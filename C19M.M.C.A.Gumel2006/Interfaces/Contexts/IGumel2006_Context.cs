bcb19b45173968eb4a095c3780307e2c6256bc6c
Justin Britt <britt@uwindsor.ca>
2020-04-15 15:30:14 -0400

    Added properties

--- C19M.M.C.A.Gumel2006/Interfaces/Contexts/IGumel2006_Context.cs
+++ C19M.M.C.A.Gumel2006/Interfaces/Contexts/IGumel2006_Context.cs
@@ -8,5 +8,59 @@
 
     public interface IGumel2006_Context
     {
+        /// <summary>
+        /// Gets the disease-induced mortality rate.
+        /// Parameter: d
+        /// Units: Per day
+        /// </summary>
+        double DiseaseInducedMortalityRate { get; }
+
+        /// <summary>
+        /// Gets the rate of development of clinical symptoms.
+        /// Parameter: α
+        /// Units: Per day
+        /// </summary>
+        double DevelopmentClinicalSymptomsRate { get; }
+
+        /// <summary>
+        /// Gets the effective contact rate.
+        /// Parameter: β
+        /// Units: Per day
+        /// </summary>
+        double EffectiveContactRate { get; }
+
+        /// <summary>
+        /// Gets the recovery rate.
+        /// Parameter: δ
+        /// Units: Per day
+        /// </summary>
+        double RecoveryRate { get; }
+
+        /// <summary>
+        /// Gets the natural mortality rate.
+        /// Parameter: μ
+        /// Units: Per day
+        /// </summary>
+        double NaturalMortalityRate { get; }
+
+        /// <summary>
+        /// Gets the vaccination coverage rate.
+        /// Parameter: ξ
+        /// Units: Per day
+        /// </summary>
+        double VaccinationCoverageRate { get; }
+
+        /// <summary>
+        /// Gets the recruitment rate of susceptible humans.
+        /// Parameter: Π
+        /// Units: Per day
+        /// </summary>
+        double RecruitmentRateSusceptibleHumans { get; }
+
+        /// <summary>
+        /// Gets the efficacy of the vaccine.
+        /// Parameter: τ
+        /// </summary>
+        double VaccineEfficacy { get; }
     }
 }
