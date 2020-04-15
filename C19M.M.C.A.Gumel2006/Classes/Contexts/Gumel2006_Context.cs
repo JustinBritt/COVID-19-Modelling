372e5255ddf65152fe1f0e47ac303a183c3fec24
Justin Britt <britt@uwindsor.ca>
2020-04-15 14:17:32 -0400

    Added property

--- C19M.M.C.A.Gumel2006/Classes/Contexts/Gumel2006_Context.cs
+++ C19M.M.C.A.Gumel2006/Classes/Contexts/Gumel2006_Context.cs
@@ -16,9 +16,19 @@
         private ILog Log { get; }
 
         // Constructor
-        public Gumel2006_Context()
+        public Gumel2006_Context(
+            double diseaseInducedMortalityRate)
         {
             this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.DiseaseInducedMortalityRate = diseaseInducedMortalityRate;
         }
+
+        /// <summary>
+        /// Gets the disease-induced mortality rate.
+        /// Parameter: d
+        /// Units: Per day
+        /// </summary>
+        public double DiseaseInducedMortalityRate { get; }
     }
 }
