ca5ccd0203e92016045fa041b876e77a18edaebe
Justin Britt <britt@uwindsor.ca>
2020-04-15 10:47:43 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Variables/VaccinatedIndividuals/V.cs
@@ -0,0 +1,29 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Variables.VaccinatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.VaccinatedIndividuals;
+
+    internal sealed class V : IV
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public V(
+            double initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public double InitialValue { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Variables/VaccinatedIndividuals/IV.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Variables.VaccinatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IV
+    {
+        double InitialValue { get; }
+    }
+}
