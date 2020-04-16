25d39c4d86921397b7e0f19efa372070a78dea8c
Justin Britt <britt@uwindsor.ca>
2020-04-16 02:03:04 -0400

    Added  project

---
+++ C19M.M.C.A.Gumel2006.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("C19M.M.C.A.Gumel2006.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C19M.M.C.A.Gumel2006.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db519620-cc47-473f-8680-fc2d462b3958")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ C19M.M.C.A.Gumel2006.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿namespace C19M.M.C.A.Gumel2006.Tests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
