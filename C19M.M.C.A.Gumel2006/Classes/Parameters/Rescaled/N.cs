1505704e62fc57b430623fc80e88000d1c6b618c
Justin Britt <britt@uwindsor.ca>
2020-04-16 01:46:04 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/Rescaled/N.cs
@@ -0,0 +1,29 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.Rescaled
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.Rescaled;
+
+    internal sealed class N : IN
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public N(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/Rescaled/IN.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.Rescaled
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IN
+    {
+        double Value { get; }
+    }
+}
