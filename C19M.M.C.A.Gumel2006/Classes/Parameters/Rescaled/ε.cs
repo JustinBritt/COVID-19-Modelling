cbb61e2c868ce3625bc05dc0bf08e04aae73fce4
Justin Britt <britt@uwindsor.ca>
2020-04-16 01:51:05 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Parameters/Rescaled/ε.cs
@@ -0,0 +1,29 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Parameters.Rescaled
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.Rescaled;
+
+    internal sealed class ε : Iε
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public ε(
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Parameters/Rescaled/Iε.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Parameters.Rescaled
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface Iε
+    {
+        double Value { get; }
+    }
+}
