ba47f9c646dc453059f043f5c2aec30be8f2274b
Justin Britt <britt@uwindsor.ca>
2020-04-16 00:20:11 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Models/RescaledDiseaseTransmission_Model.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Models;
+
+    internal sealed class RescaledDiseaseTransmission_Model : IRescaledDiseaseTransmission_Model
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public RescaledDiseaseTransmission_Model()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Models/IRescaledDiseaseTransmission_Model.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IRescaledDiseaseTransmission_Model
+    {
+    }
+}
