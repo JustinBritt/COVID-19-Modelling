65bf30f481f3ae4a2df3bdde572c35ecbe7e0a12
Justin Britt <britt@uwindsor.ca>
2020-04-16 11:39:08 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Models/EpidemicOutbreak_Model.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Contexts;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Models;
+
+    internal sealed class EpidemicOutbreak_Model : IEpidemicOutbreak_Model
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public EpidemicOutbreak_Model(
+            IGumel2006_Context context)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
