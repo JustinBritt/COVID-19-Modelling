569941adc5f9f6d536163c97b093df9b21757ade
Justin Britt <britt@uwindsor.ca>
2020-04-16 00:40:13 -0400

    Changes to usings

--- C19M.M.C.A.Gumel2006/Classes/Models/RescaledDiseaseTransmission_Model.cs
+++ C19M.M.C.A.Gumel2006/Classes/Models/RescaledDiseaseTransmission_Model.cs
@@ -9,7 +9,35 @@
     // Logging
     using log4net;
 
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.DevelopmentClinicalSymptomsRate;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.DiseaseInducedMortalityRate;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.EffectiveContactRate;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.EquilibriumPopulationWithoutDisease;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.NaturalMortalityRate;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.RecoveryRate;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.RecruitmentRateSusceptibleHumans;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.VaccinationCoverageRate;
+    using C19M.M.C.A.Gumel2006.Classes.Parameters.VaccineEfficacy;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Contexts;
+
     using C19M.M.C.A.Gumel2006.Interfaces.Models;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.DevelopmentClinicalSymptomsRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.DiseaseInducedMortalityRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.EffectiveContactRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.EquilibriumPopulationWithoutDisease;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.NaturalMortalityRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecoveryRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecruitmentRateSusceptibleHumans;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.VaccinationCoverageRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.VaccineEfficacy;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.InfectedIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.LatentIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.SusceptibleIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.TreatedIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.VaccinatedIndividuals;
 
     internal sealed class RescaledDiseaseTransmission_Model : IRescaledDiseaseTransmission_Model
     {
