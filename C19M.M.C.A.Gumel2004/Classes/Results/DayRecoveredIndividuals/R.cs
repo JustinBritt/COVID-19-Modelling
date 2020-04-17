e08d2ad643aca82441439cd4b7b77494f453fd0c
Justin Britt <britt@uwindsor.ca>
2020-04-17 12:21:16 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Results/DayRecoveredIndividuals/R.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Results.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayRecoveredIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayRecoveredIndividuals;
+
+    internal sealed class R : IR
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R(
+            ImmutableList<IR_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IR_ResultElement> Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/Results/DayRecoveredIndividuals/IR.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Results.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayRecoveredIndividuals;
+
+    internal interface IR
+    {
+        ImmutableList<IR_ResultElement> Value { get; }
+    }
+}
