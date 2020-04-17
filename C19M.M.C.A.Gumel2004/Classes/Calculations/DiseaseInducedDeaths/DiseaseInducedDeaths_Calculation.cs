c777bd10fcf159e6f166eeeba088c4f24a73f9ed
Justin Britt <britt@uwindsor.ca>
2020-04-17 03:29:04 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DiseaseInducedDeaths/DiseaseInducedDeaths_Calculation.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DiseaseInducedDeaths;
+
+    internal sealed class DiseaseInducedDeaths_Calculation : IDiseaseInducedDeaths_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DiseaseInducedDeaths_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
