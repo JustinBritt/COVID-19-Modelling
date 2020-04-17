13625dcd570d22170c6fb7b68d2e21a7ed26da92
Justin Britt <britt@uwindsor.ca>
2020-04-17 02:42:34 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Solutions/DiseaseTransmission_Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Solutions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Solutions;
+
+    internal sealed class DiseaseTransmission_Solution : IDiseaseTransmission_Solution
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DiseaseTransmission_Solution()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
