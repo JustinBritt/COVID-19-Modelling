0051e258ec22705282098ee42d564a296f9bc425
Justin Britt <britt@uwindsor.ca>
2020-04-17 07:57:58 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/ResultElements/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths_ResultElement.cs
@@ -0,0 +1,37 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.ResultElements.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    internal sealed class DayCumulativeDiseaseInducedDeaths_ResultElement : IDayCumulativeDiseaseInducedDeaths_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeDiseaseInducedDeaths_ResultElement(
+            DateTime t_IndexElement,
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public DateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the cumulative number of disease-induced deaths.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/ResultElements/DayCumulativeDiseaseInducedDeaths/IDayCumulativeDiseaseInducedDeaths_ResultElement.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IDayCumulativeDiseaseInducedDeaths_ResultElement
+    {
+        DateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the cumulative number of disease-induced deaths.
+        /// </summary>
+        double Value { get; }
+    }
+}
