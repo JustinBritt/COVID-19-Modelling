48da36f1e7dbaa85cb1f9ce8cdbe559a3e757d13
Justin Britt <britt@uwindsor.ca>
2020-04-17 12:28:46 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Results/DaySusceptibleIndividuals/S.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Results.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DaySusceptibleIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DaySusceptibleIndividuals;
+
+    internal sealed class S : IS
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S(
+            ImmutableList<IS_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IS_ResultElement> Value { get; }
+    }
+}
