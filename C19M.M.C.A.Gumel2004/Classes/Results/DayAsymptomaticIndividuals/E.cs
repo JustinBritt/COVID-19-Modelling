63ea7d6c85c566eb995caf72ecfc72c92e330514
Justin Britt <britt@uwindsor.ca>
2020-04-17 11:16:51 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Results/DayAsymptomaticIndividuals/E.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Results.DayAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayAsymptomaticIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayAsymptomaticIndividuals;
+
+    internal sealed class E : IE
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E(
+            ImmutableList<IE_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IE_ResultElement> Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/Results/DayAsymptomaticIndividuals/IE.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Results.DayAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayAsymptomaticIndividuals;
+
+    internal interface IE
+    {
+        ImmutableList<IE_ResultElement> Value { get; }
+    }
+}
