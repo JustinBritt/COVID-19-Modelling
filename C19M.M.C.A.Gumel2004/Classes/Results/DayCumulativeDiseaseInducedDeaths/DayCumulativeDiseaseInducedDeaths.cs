54882af7d0ef4d4e2ecc6e3044063cdd60576292
Justin Britt <britt@uwindsor.ca>
2020-04-17 08:00:00 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Results/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Results.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayCumulativeDiseaseInducedDeaths;
+
+    internal sealed class DayCumulativeDiseaseInducedDeaths : IDayCumulativeDiseaseInducedDeaths
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeDiseaseInducedDeaths(
+            ImmutableList<IDayCumulativeDiseaseInducedDeaths_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IDayCumulativeDiseaseInducedDeaths_ResultElement> Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/Results/DayCumulativeDiseaseInducedDeaths/IDayCumulativeDiseaseInducedDeaths.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Results.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    internal interface IDayCumulativeDiseaseInducedDeaths
+    {
+        ImmutableList<IDayCumulativeDiseaseInducedDeaths_ResultElement> Value { get; }
+    }
+}
