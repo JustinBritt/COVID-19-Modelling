63eaeffef938dc9a9c52a0604d7f798ad51f4396
Justin Britt <britt@uwindsor.ca>
2020-04-17 15:31:19 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayAsymptomaticIndividuals/E_Calculation.cs
@@ -0,0 +1,42 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DayAsymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayAsymptomaticIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayAsymptomaticIndividuals;
+
+    internal sealed class E_Calculation : IE_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IE Calculate(
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2004.Classes.Results.DayAsymptomaticIndividuals.E(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Gumel2004.Classes.Calculations.DayAsymptomaticIndividuals.E_ResultElement_Calculation().Calculate(
+                        w,
+                        t,
+                        RungeKuttaResults))
+                .ToImmutableList());
+        }
+    }
+}
