fa44a56673374da7cd8d45f82549773ee2152128
Justin Britt <britt@uwindsor.ca>
2020-04-17 15:43:08 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayIsolatedIndividuals/J_ResultElement_Calculation.cs
@@ -0,0 +1,38 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DayIsolatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayIsolatedIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayIsolatedIndividuals;
+
+    internal sealed class J_ResultElement_Calculation : IJ_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public J_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IJ_ResultElement Calculate(
+            DateTime t_IndexElement,
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2004.Classes.ResultElements.DayIsolatedIndividuals.J_ResultElement(
+                t_IndexElement,
+                RungeKuttaResults[t.NumberDaysAfterStartDate.Invoke(t_IndexElement)][2]);
+        }
+    }
+}
