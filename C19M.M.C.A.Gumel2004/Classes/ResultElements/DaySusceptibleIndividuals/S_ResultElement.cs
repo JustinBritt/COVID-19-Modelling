d3db31aacfe8339db7a8181c7e5839d3bbcf16d6
Justin Britt <britt@uwindsor.ca>
2020-04-17 12:05:31 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/ResultElements/DaySusceptibleIndividuals/S_ResultElement.cs
@@ -0,0 +1,34 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.ResultElements.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DaySusceptibleIndividuals;
+
+    internal sealed class S_ResultElement : IS_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S_ResultElement(
+            DateTime t_IndexElement,
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public DateTime t_IndexElement { get; }
+
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/ResultElements/DaySusceptibleIndividuals/IS_ResultElement.cs
@@ -0,0 +1,15 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IS_ResultElement
+    {
+        DateTime t_IndexElement { get; }
+
+        double Value { get; }
+    }
+}
