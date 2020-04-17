e46199bd5c0b50eec46f34fe66d31e176f90f18b
Justin Britt <britt@uwindsor.ca>
2020-04-17 11:40:23 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Interfaces/ResultElements/DaySymptomaticIndividuals/II_ResultElement.cs
@@ -0,0 +1,15 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DaySymptomaticIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface II_ResultElement
+    {
+        DateTime t_IndexElement { get; }
+
+        double Value { get; }
+    }
+}
