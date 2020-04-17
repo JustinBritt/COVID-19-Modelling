3bc9154309ce7fe5274457e9896b0cb5226fdec0
Justin Britt <britt@uwindsor.ca>
2020-04-17 11:56:02 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Interfaces/ResultElements/DayIsolatedIndividuals/IJ_ResultElement.cs
@@ -0,0 +1,15 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayIsolatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IJ_ResultElement
+    {
+        DateTime t_IndexElement { get; }
+
+        double Value { get; }
+    }
+}
