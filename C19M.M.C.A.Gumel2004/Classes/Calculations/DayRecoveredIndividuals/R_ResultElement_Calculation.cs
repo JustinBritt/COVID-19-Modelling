d9d7c5da9dc86eaa9054e2001ed1f0de18177143
Justin Britt <britt@uwindsor.ca>
2020-04-18 04:08:58 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayRecoveredIndividuals/R_ResultElement_Calculation.cs
@@ -0,0 +1,38 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayRecoveredIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayRecoveredIndividuals;
+
+    internal sealed class R_ResultElement_Calculation : IR_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IR_ResultElement Calculate(
+            DateTime t_IndexElement,
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2004.Classes.ResultElements.DayRecoveredIndividuals.R_ResultElement(
+                t_IndexElement,
+                RungeKuttaResults[t.NumberDaysAfterStartDate.Invoke(t_IndexElement)][4]);
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/Calculations/DayRecoveredIndividuals/IR_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IR_ResultElement_Calculation
+    {
+    }
+}
