1e34ce1c617734c51cf6141b31e9dd5cbcc6e8f3
Justin Britt <britt@uwindsor.ca>
2020-04-18 04:17:31 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Interfaces/Calculations/DaySusceptibleIndividuals/IS_Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Calculations.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IS_Calculation
+    {
+    }
+}
