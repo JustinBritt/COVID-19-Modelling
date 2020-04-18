7d08e4002e22c3b95dd93de9e7482625868f6fc9
Justin Britt <britt@uwindsor.ca>
2020-04-18 12:17:21 -0400

    Changes to Solve method

--- C19M.M.C.A.Gumel2004/Classes/Solutions/Gumel2004_Solution.cs
+++ C19M.M.C.A.Gumel2004/Classes/Solutions/Gumel2004_Solution.cs
@@ -78,30 +78,10 @@
         public C19M.M.C.A.Gumel2004.Interfaces.Results.DaySusceptibleIndividuals.IS S { get; private set; }
 
         public double Solve(
-            It t,
-            Interfaces.Parameters.DiseaseInducedDeathRateSymptomaticIndividuals.Id d_1,
-            Interfaces.Parameters.DiseaseInducedDeathRateIsolatedIndividuals.Id d_2,
-            Ip p,
-            Iβ β,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.QuarantineRateAsymptomaticIndividuals.Iγ γ_1,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.IsolationRateSymptomaticIndividuals.Iγ γ_2,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorAsymptomaticIndividuals.Iε ε_E,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorIsolatedIndividuals.Iε ε_J,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.TransmissionCoefficientModificationFactorQuarantinedIndividuals.Iε ε_Q,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals.Iκ κ_1,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.DevelopmentClinicalSymptomsRateQuarantinedIndividuals.Iκ κ_2,
-            Iμ μ,
-            IΠ Π,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.RecoveryRateSymptomaticIndividuals.Iσ σ_1,
-            C19M.M.C.A.Gumel2004.Interfaces.Parameters.RecoveryRateIsolatedIndividuals.Iσ σ_2,
-            C19M.M.C.A.Gumel2004.Interfaces.Variables.AsymptomaticIndividuals.IE E,
-            C19M.M.C.A.Gumel2004.Interfaces.Variables.SymptomaticIndividuals.II I,
-            C19M.M.C.A.Gumel2004.Interfaces.Variables.IsolatedIndividuals.IJ J,
-            C19M.M.C.A.Gumel2004.Interfaces.Variables.QuarantinedIndividuals.IQ Q,
-            C19M.M.C.A.Gumel2004.Interfaces.Variables.RecoveredIndividuals.IR R,
-            C19M.M.C.A.Gumel2004.Interfaces.Variables.SusceptibleIndividuals.IS S)
+            IGumel2004_Context context)
         {
-            // IGumel2004_Model model = new C19M.M.C.A.Gumel2004.Classes.Models.Gumel2004_Model();
+            IGumel2004_Model model = new C19M.M.C.A.Gumel2004.Classes.Models.Gumel2004_Model(
+                context);
 
             MathNet.Numerics.LinearAlgebra.Vector<double>[] results = 
                 MathNet.Numerics.OdeSolvers.RungeKutta.FourthOrder(
