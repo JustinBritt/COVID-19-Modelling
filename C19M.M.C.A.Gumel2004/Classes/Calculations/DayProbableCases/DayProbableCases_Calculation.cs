35482edf675733cc5a1439aa36fcb2cc5a9bf327
Justin Britt <britt@uwindsor.ca>
2020-04-18 14:26:59 -0400

    Added Calculate method

--- C19M.M.C.A.Gumel2004/Classes/Calculations/DayProbableCases/DayProbableCases_Calculation.cs
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayProbableCases/DayProbableCases_Calculation.cs
@@ -12,6 +12,14 @@
 
     using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayProbableCases;
 
+    using C19M.M.C.A.Gumel2004.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayIsolatedIndividuals;
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayProbableCases;
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayQuarantinedIndividuals;
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayRecoveredIndividuals;
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DaySymptomaticIndividuals;
+
     internal sealed class DayProbableCases_Calculation : IDayProbableCases_Calculation
     {
         private ILog Log { get; }
@@ -20,6 +28,24 @@
         public DayProbableCases_Calculation()
         {
             this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IDayProbableCases Calculate(
+            It t,
+            II I,
+            IJ J,
+            IQ Q,
+            IR R)
+        {
+            return new C19M.M.C.A.Gumel2004.Classes.Results.DayProbableCases.DayProbableCases(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Gumel2004.Classes.Calculations.DayProbableCases.DayProbableCases_ResultElement_Calculation().Calculate(
+                        w,
+                        I,
+                        J,
+                        Q,
+                        R))
+                .ToImmutableList());
         }
     }
 }
