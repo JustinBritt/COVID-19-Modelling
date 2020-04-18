998e472118f338544cf0456c112c0653ca352309
Justin Britt <britt@uwindsor.ca>
2020-04-18 13:35:37 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Results/DayCumulativeProbableCases/DayCumulativeProbableCases.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Results.DayCumulativeProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeProbableCases;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayCumulativeProbableCases;
+
+    internal sealed class DayCumulativeProbableCases : IDayCumulativeProbableCases
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeProbableCases(
+            ImmutableList<IDayCumulativeProbableCases_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IDayCumulativeProbableCases_ResultElement> Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/Results/DayCumulativeProbableCases/IDayCumulativeProbableCases.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Results.DayCumulativeProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeProbableCases;
+
+    internal interface IDayCumulativeProbableCases
+    {
+        ImmutableList<IDayCumulativeProbableCases_ResultElement> Value { get; }
+    }
+}
