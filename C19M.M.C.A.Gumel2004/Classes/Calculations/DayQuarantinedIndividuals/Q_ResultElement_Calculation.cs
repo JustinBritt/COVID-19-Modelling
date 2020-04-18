9c27fda8be849b2e0304dadd06e73bff392b3ede
Justin Britt <britt@uwindsor.ca>
2020-04-18 03:38:15 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayQuarantinedIndividuals/Q_ResultElement_Calculation.cs
@@ -0,0 +1,38 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DayQuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayQuarantinedIndividuals;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayQuarantinedIndividuals;
+
+    internal sealed class Q_ResultElement_Calculation : IQ_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Q_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IQ_ResultElement Calculate(
+            DateTime t_IndexElement,
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2004.Classes.ResultElements.DayQuarantinedIndividuals.Q_ResultElement(
+                t_IndexElement,
+                RungeKuttaResults[t.NumberDaysAfterStartDate.Invoke(t_IndexElement)][3]);
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/Calculations/DayQuarantinedIndividuals/IQ_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayQuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IQ_ResultElement_Calculation
+    {
+    }
+}
