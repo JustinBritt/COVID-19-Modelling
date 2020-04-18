ec3b414c0844c5c6349ad564f38761aff532f77c
Justin Britt <britt@uwindsor.ca>
2020-04-18 13:26:44 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/ResultElements/DayProbableCases/DayProbableCases_ResultElement.cs
@@ -0,0 +1,37 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.ResultElements.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayProbableCases;
+
+    internal sealed class DayProbableCases_ResultElement : IDayProbableCases_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayProbableCases_ResultElement(
+            DateTime t_IndexElement,
+            double value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public DateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the number of probable cases.
+        /// </summary>
+        public double Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Gumel2004/Interfaces/ResultElements/DayProbableCases/IDayProbableCases_ResultElement.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IDayProbableCases_ResultElement
+    {
+        DateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the number of probable cases.
+        /// </summary>
+        double Value { get; }
+    }
+}
