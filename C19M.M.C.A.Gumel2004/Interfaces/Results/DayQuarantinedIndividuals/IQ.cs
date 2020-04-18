325b450a136b0e531b332b10f4cc7a20edcf2e5a
Justin Britt <britt@uwindsor.ca>
2020-04-18 14:23:22 -0400

    Added lookup method

--- C19M.M.C.A.Gumel2004/Interfaces/Results/DayQuarantinedIndividuals/IQ.cs
+++ C19M.M.C.A.Gumel2004/Interfaces/Results/DayQuarantinedIndividuals/IQ.cs
@@ -12,5 +12,8 @@
     internal interface IQ
     {
         ImmutableList<IQ_ResultElement> Value { get; }
+
+        double GetElementAt(
+            DateTime t_IndexElement);
     }
 }
--- C19M.M.C.A.Gumel2004/Interfaces/Results/DaySusceptibleIndividuals/IS.cs
+++ C19M.M.C.A.Gumel2004/Interfaces/Results/DaySusceptibleIndividuals/IS.cs
@@ -12,5 +12,8 @@
     internal interface IS
     {
         ImmutableList<IS_ResultElement> Value { get; }
+
+        double GetElementAt(
+            DateTime t_IndexElement);
     }
 }
