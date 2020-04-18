9f4b6f3e661dc2ec92e3be35016bb78eca7db789
Justin Britt <britt@uwindsor.ca>
2020-04-18 06:39:21 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation.cs
@@ -0,0 +1,28 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Results.DayDiseaseInducedDeaths;
+
+    internal sealed class DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation : IDayCumulativeDiseaseInducedDeaths_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
