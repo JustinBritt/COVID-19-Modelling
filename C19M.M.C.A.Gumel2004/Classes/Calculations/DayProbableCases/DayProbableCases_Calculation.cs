2d7e3e3157042f2eba11dca8b5d76c29a8d94cf4
Justin Britt <britt@uwindsor.ca>
2020-04-18 13:45:45 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2004/Classes/Calculations/DayProbableCases/DayProbableCases_Calculation.cs
@@ -0,0 +1,25 @@
+﻿namespace C19M.M.C.A.Gumel2004.Classes.Calculations.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Calculations.DayProbableCases;
+
+    internal sealed class DayProbableCases_Calculation : IDayProbableCases_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayProbableCases_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
