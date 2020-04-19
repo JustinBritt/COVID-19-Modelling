088018377fa86a486e7a13da1491f95845be262a
Justin Britt <britt@uwindsor.ca>
2020-04-19 12:00:34 -0400

    Use Hl7.Fhir types

--- C19M.D.Gumel2004/Interfaces/IGreaterTorontoArea.cs
+++ C19M.D.Gumel2004/Interfaces/IGreaterTorontoArea.cs
@@ -17,27 +17,27 @@
         /// <summary>
         /// Gets the start date.
         /// </summary>
-        DateTime StartDate { get; }
+        FhirDateTime StartDate { get; }
 
         /// <summary>
         /// Gets the start date for isolation and quarantine programs.
         /// </summary>
-        DateTime IsolationQuarantineStartDate { get; }
+        FhirDateTime IsolationQuarantineStartDate { get; }
 
         /// <summary>
         /// Gets the start date for perfect isolation.
         /// </summary>
-        DateTime PerfectIsolationStartDate { get; }
+        FhirDateTime PerfectIsolationStartDate { get; }
 
         /// <summary>
         /// Gets the end date.
         /// </summary>
-        DateTime EndDate { get; }
+        FhirDateTime EndDate { get; }
 
         /// <summary>
         /// Gets the number of days after the start date for some other date.
         /// </summary>
-        Func<DateTime, int> NumberDaysAfterStartDate { get; }
+        Func<FhirDateTime, PositiveInt> NumberDaysAfterStartDate { get; }
 
         /// <summary>
         /// Gets the disease-induced death rate for symptomatic individuals.
@@ -57,123 +57,123 @@
         /// Gets the initial value for asymptomatic individuals.
         /// Parameter: E(0)
         /// </summary>
-        double InitialValueAsymptomaticIndividuals { get; }
+        FhirDecimal InitialValueAsymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the initial value for symptomatic individuals.
         /// Parameter: I(0)
         /// </summary>
-        double InitialValueSymptomaticIndividuals { get; }
+        FhirDecimal InitialValueSymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the initial value for isolated individuals.
         /// Parameter: J(0)
         /// </summary>
-        double InitialValueIsolatedIndividuals { get; }
+        FhirDecimal InitialValueIsolatedIndividuals { get; }
 
         /// <summary>
         /// Gets the recruitment rate of asymptomatic individuals per day.
         /// Parameter: p
         /// Units: Per day
         /// </summary>
-        double RecruitmentRateAsymptomaticIndividuals { get; }
+        FhirDecimal RecruitmentRateAsymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the initial value for quarantined individuals.
         /// Parameter: Q(0)
         /// </summary>
-        double InitialValueQuarantinedIndividuals { get; }
+        FhirDecimal InitialValueQuarantinedIndividuals { get; }
 
         /// <summary>
         /// Gets the initial value for recovered individuals.
         /// Parameter: R(0)
         /// </summary>
-        double InitialValueRecoveredIndividuals { get; }
+        FhirDecimal InitialValueRecoveredIndividuals { get; }
 
         /// <summary>
         /// Gets the initial value for susceptible individuals.
         /// Parameter: S(0)
         /// </summary>
-        double InitialValueSusceptibleIndividuals { get; }
+        FhirDecimal InitialValueSusceptibleIndividuals { get; }
 
         /// <summary>
         /// Gets the basic transmission coefficient.
         /// Parameter: β
         /// </summary>
-        double BasicTransmissionCoefficient { get; }
+        FhirDecimal BasicTransmissionCoefficient { get; }
 
         /// <summary>
         /// Gets the quarantine rate for asymptomatic individuals.
         /// Parameter: γ_1
         /// Units: Per day
         /// </summary>
-        Func<DateTime, double> QuarantineRateAsymptomaticIndividuals { get; }
+        Func<FhirDateTime, FhirDecimal> QuarantineRateAsymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the isolation rate for symptomatic individuals.
         /// Parameter: γ_2
         /// Units: Per day
         /// </summary>
-        Func<DateTime, double> IsolationRateSymptomaticIndividuals { get; }
+        Func<FhirDateTime, FhirDecimal> IsolationRateSymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the transmission coefficient modification factor for asymptomatic individuals.
         /// Parameter: ε_E
         /// </summary>
-        double TransmissionCoefficientModificationFactorAsymptomaticIndividuals { get; }
+        FhirDecimal TransmissionCoefficientModificationFactorAsymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the transmission coefficient modification factor for isolated individuals.
         /// Parameter: ε_J
         /// </summary>
-        Func<DateTime, double> TransmissionCoefficientModificationFactorIsolatedIndividuals { get; }
+        Func<FhirDateTime, FhirDecimal> TransmissionCoefficientModificationFactorIsolatedIndividuals { get; }
 
         /// <summary>
         /// Gets the transmission coefficient modification factor for quarantined individuals.
         /// Parameter: ε_Q
         /// </summary>
-        Func<DateTime, double> TransmissionCoefficientModificationFactorQuarantinedIndividuals { get; }
+        Func<FhirDateTime, FhirDecimal> TransmissionCoefficientModificationFactorQuarantinedIndividuals { get; }
 
         /// <summary>
         /// Gets the rate at which asymptomatic individuals develop clinical symptoms.
         /// Parameter: κ_1
         /// Units: Per day
         /// </summary>
-        double DevelopmentClinicalSymptomsRateAsymptomaticIndividuals { get; }
+        FhirDecimal DevelopmentClinicalSymptomsRateAsymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the rate at which quarantined individuals develop clinical symptoms.
         /// Parameter: κ_2
         /// Units: Per day
         /// </summary>
-        double DevelopmentClinicalSymptomsRateQuarantinedIndividuals { get; }
+        FhirDecimal DevelopmentClinicalSymptomsRateQuarantinedIndividuals { get; }
 
         /// <summary>
         /// Gets the natural death rate.
         /// Parameter: μ
         /// Units: Per day
         /// </summary>
-        double NaturalDeathRate { get; }
+        FhirDecimal NaturalDeathRate { get; }
 
         /// <summary>
         /// Gets the net inflow rate of susceptible individuals per unit time.
         /// Parameter: Π
         /// Units: Per day
         /// </summary>
-        double NetInflowRateSusceptibleIndividuals { get; }
+        FhirDecimal NetInflowRateSusceptibleIndividuals { get; }
 
         /// <summary>
         /// Gets the recovery rate for symptomatic individuals.
         /// Parameter: σ_1
         /// Units: Per day
         /// </summary>
-        double RecoveryRateSymptomaticIndividuals { get; }
+        FhirDecimal RecoveryRateSymptomaticIndividuals { get; }
 
         /// <summary>
         /// Gets the recovery rate for isolated individuals.
         /// Parameter: σ_2
         /// Units: Per day
         /// </summary>
-        double RecoveryRateIsolatedIndividuals { get; }
+        FhirDecimal RecoveryRateIsolatedIndividuals { get; }
     }
 }
