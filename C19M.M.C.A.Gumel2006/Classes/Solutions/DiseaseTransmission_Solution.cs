e540cf042bc26d07cf6c155b1bbf44aa9ae8c14e
Justin Britt <britt@uwindsor.ca>
2020-04-19 17:34:18 -0400

    Changes to f

--- C19M.M.C.A.Gumel2006/Classes/Solutions/DiseaseTransmission_Solution.cs
+++ C19M.M.C.A.Gumel2006/Classes/Solutions/DiseaseTransmission_Solution.cs
@@ -34,6 +34,7 @@
         private Func<double, MathNet.Numerics.LinearAlgebra.Vector<double>, MathNet.Numerics.LinearAlgebra.Vector<double>> f(
             C19M.M.C.A.Gumel2006.Interfaces.Parameters.DiseaseInducedMortalityRate.Id d,
             C19M.M.C.A.Gumel2006.Interfaces.Parameters.DevelopmentClinicalSymptomsRate.Iα α,
+            C19M.M.C.A.Gumel2006.Interfaces.Parameters.EffectiveContactRate.Iβ β,
             C19M.M.C.A.Gumel2006.Interfaces.Variables.LatentIndividuals.IE E,
             C19M.M.C.A.Gumel2006.Interfaces.Variables.InfectedIndividuals.II I,
             C19M.M.C.A.Gumel2006.Interfaces.Variables.TotalPopulationSize.IN N,
