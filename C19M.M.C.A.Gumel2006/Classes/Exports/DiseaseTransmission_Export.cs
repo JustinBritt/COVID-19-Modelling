5b0fdadcdd69563a2f21dc1362fafe5cf8fd545d
Justin Britt <britt@uwindsor.ca>
2020-04-20 12:37:29 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Exports/DiseaseTransmission_Export.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Exports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Exports;
+
+    public sealed class DiseaseTransmission_Export : IDiseaseTransmission_Export
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DiseaseTransmission_Export()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Exports/IDiseaseTransmission_Export.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Exports
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface IDiseaseTransmission_Export
+    {
+    }
+}
