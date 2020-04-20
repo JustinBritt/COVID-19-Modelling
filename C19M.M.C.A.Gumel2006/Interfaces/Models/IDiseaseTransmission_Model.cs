5ff52d72fded9e1b217614d95931b9ba3ecf9aeb
Justin Britt <britt@uwindsor.ca>
2020-04-19 20:13:26 -0400

    Added properties

--- C19M.M.C.A.Gumel2006/Interfaces/Models/IDiseaseTransmission_Model.cs
+++ C19M.M.C.A.Gumel2006/Interfaces/Models/IDiseaseTransmission_Model.cs
@@ -6,7 +6,46 @@
     using System.Text;
     using System.Threading.Tasks;
 
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.DevelopmentClinicalSymptomsRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.EffectiveContactRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.NaturalMortalityRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecoveryRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.RecruitmentRateSusceptibleHumans;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.VaccinationCoverageRate;
+    using C19M.M.C.A.Gumel2006.Interfaces.Parameters.VaccineEfficacy;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.InfectedIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.LatentIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.SusceptibleIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.TreatedIndividuals;
+    using C19M.M.C.A.Gumel2006.Interfaces.Variables.VaccinatedIndividuals;
+
     internal interface IDiseaseTransmission_Model
     {
+        C19M.M.C.A.Gumel2006.Interfaces.Parameters.DiseaseInducedMortalityRate.Id d { get; }
+
+        Iα α { get; }
+
+        Iβ β { get; }
+
+        Iδ δ { get; }
+
+        Iμ μ { get; }
+
+        Iξ ξ { get; }
+
+        IΠ Π { get; }
+
+        Iτ τ { get; }
+
+        IE E { get; }
+
+        II I { get; }
+
+        IR R { get; }
+
+        IS S { get; }
+
+        IV V { get; }
     }
 }
