e89bb3dea5f74e8e21e4b22da4652b79b733c34b
Justin Britt <britt@uwindsor.ca>
2020-04-21 08:45:53 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/Results/DayTreatedIndividuals/IR.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Results.DayTreatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayTreatedIndividuals;
+
+    internal interface IR
+    {
+        ImmutableList<IR_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
