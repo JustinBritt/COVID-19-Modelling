550e089963d501f60d8c1f59f8025f760098503a
Justin Britt <britt@uwindsor.ca>
2020-04-21 15:05:54 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/Calculations/DayLatentIndividuals/IE_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayLatentIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IE_ResultElement_Calculation
+    {
+    }
+}
