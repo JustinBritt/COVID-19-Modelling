0e109b4594c969408ec188a7c3884ad96fc6beec
Justin Britt <britt@uwindsor.ca>
2020-04-21 15:38:35 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Calculations/DayVaccinatedIndividuals/V_ResultElement_Calculation.cs
@@ -0,0 +1,42 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Calculations.DayVaccinatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayVaccinatedIndividuals;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayVaccinatedIndividuals;
+
+    internal sealed class V_ResultElement_Calculation : IV_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public V_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IV_ResultElement Calculate(
+            FhirDateTime t_IndexElement,
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2006.Classes.ResultElements.DayVaccinatedIndividuals.V_ResultElement(
+                t_IndexElement,
+                new FhirDecimal(
+                    (decimal)RungeKuttaResults[t.NumberDaysAfterStartDate.Invoke(t_IndexElement).Value.Value][4]));
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Calculations/DayVaccinatedIndividuals/IV_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayVaccinatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IV_ResultElement_Calculation
+    {
+    }
+}
