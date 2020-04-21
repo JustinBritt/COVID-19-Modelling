9bec67aa92d9677053ce91d77669e3f1c0e10350
Justin Britt <britt@uwindsor.ca>
2020-04-21 08:24:42 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/ResultElements/DayInfectedIndividuals/II_ResultElement.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayInfectedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface II_ResultElement
+    {
+        FhirDateTime t_IndexElement { get; }
+
+        FhirDecimal Value { get; }
+    }
+}
