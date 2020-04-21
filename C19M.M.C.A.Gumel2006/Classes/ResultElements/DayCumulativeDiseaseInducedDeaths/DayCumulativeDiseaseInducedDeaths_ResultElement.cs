b785d6d9693ff92dac171a287f9b33dd25db2a54
Justin Britt <britt@uwindsor.ca>
2020-04-21 17:44:57 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/ResultElements/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths_ResultElement.cs
@@ -0,0 +1,40 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.ResultElements.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    internal sealed class DayCumulativeDiseaseInducedDeaths_ResultElement : IDayCumulativeDiseaseInducedDeaths_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeDiseaseInducedDeaths_ResultElement(
+            FhirDateTime t_IndexElement,
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public FhirDateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the cumulative number of disease-induced deaths.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
