176390ae998b79635a5a32427bc582e4a26ed27f
Justin Britt <britt@uwindsor.ca>
2020-04-21 18:17:36 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/ResultElements/DayProbableCases/DayProbableCases_ResultElement.cs
@@ -0,0 +1,40 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.ResultElements.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayProbableCases;
+
+    internal sealed class DayProbableCases_ResultElement : IDayProbableCases_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayProbableCases_ResultElement(
+            FhirDateTime t_IndexElement,
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public FhirDateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the number of probable cases.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
