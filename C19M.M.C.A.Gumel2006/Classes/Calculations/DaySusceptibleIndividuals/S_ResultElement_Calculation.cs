0f009cec0f0a5a6a2f26c4db3f8d2ad0b2b8e888
Justin Britt <britt@uwindsor.ca>
2020-04-21 15:15:39 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Calculations/DaySusceptibleIndividuals/S_ResultElement_Calculation.cs
@@ -0,0 +1,42 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Calculations.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Calculations.DaySusceptibleIndividuals;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DaySusceptibleIndividuals;
+
+    internal sealed class S_ResultElement_Calculation : IS_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IS_ResultElement Calculate(
+            FhirDateTime t_IndexElement,
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2006.Classes.ResultElements.DaySusceptibleIndividuals.S_ResultElement(
+                t_IndexElement,
+                new FhirDecimal(
+                    (decimal)RungeKuttaResults[t.NumberDaysAfterStartDate.Invoke(t_IndexElement).Value.Value][3]));
+        }
+    }
+}
---
+++ C19M.M.C.A.Gumel2006/Interfaces/Calculations/DaySusceptibleIndividuals/IS_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Calculations.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IS_ResultElement_Calculation
+    {
+    }
+}
