e6381d6e735e25ccc6b15fd130a3242b549ad8f4
Justin Britt <britt@uwindsor.ca>
2020-04-21 15:09:22 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Calculations/DayLatentIndividuals/E_Calculation.cs
@@ -0,0 +1,45 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Calculations.DayLatentIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayLatentIndividuals;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Results.DayLatentIndividuals;
+
+    internal sealed class E_Calculation : IE_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IE Calculate(
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2006.Classes.Results.DayLatentIndividuals.E(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Gumel2006.Classes.Calculations.DayLatentIndividuals.E_ResultElement_Calculation().Calculate(
+                        w,
+                        t,
+                        RungeKuttaResults))
+                .ToImmutableList());
+        }
+    }
+}
