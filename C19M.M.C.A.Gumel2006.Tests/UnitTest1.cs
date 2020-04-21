77d766172f772922ce12a56e0b0df789b6198afe
Justin Britt <britt@uwindsor.ca>
2020-04-21 18:46:08 -0400

    Changes related to Context

--- C19M.M.C.A.Gumel2006.Tests/UnitTest1.cs
+++ C19M.M.C.A.Gumel2006.Tests/UnitTest1.cs
@@ -16,6 +16,20 @@
             C19M.D.Gumel2006.Interfaces.IGreaterTorontoArea GreaterTorontoArea = new C19M.D.Gumel2006.Classes.GreaterTorontoArea();
 
             // Context
+            //C19M.M.C.A.Gumel2006.Interfaces.Contexts.IGumel2006_Context context = new C19M.M.C.A.Gumel2006.Classes.Contexts.Gumel2006_Context(
+            //    GreaterTorontoArea.DiseaseInducedMortalityRate,
+            //    0,
+            //    0,
+            //    0,
+            //    0,
+            //    0,
+            //    GreaterTorontoArea.DevelopmentClinicalSymptomsRate,
+            //    GreaterTorontoArea.EffectiveContactRate,
+            //    GreaterTorontoArea.RecoveryRate,
+            //    GreaterTorontoArea.NaturalMortalityRate,
+            //    GreaterTorontoArea.VaccinationCoverageRate,
+            //    GreaterTorontoArea.RecruitmentRateSusceptibleHumans,
+            //    GreaterTorontoArea.VaccineEfficacy);
 
             // Export
         }
