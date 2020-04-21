fcd2d803ae98bfc25e4448677473202888bb4164
Justin Britt <britt@uwindsor.ca>
2020-04-21 14:34:25 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Calculations/DayInfectedIndividuals/I_Calculation.cs
@@ -0,0 +1,45 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Calculations.DayInfectedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayInfectedIndividuals;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Results.DayInfectedIndividuals;
+
+    internal sealed class I_Calculation : II_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public I_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public II Calculate(
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Gumel2006.Classes.Results.DayInfectedIndividuals.I(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Gumel2006.Classes.Calculations.DayInfectedIndividuals.I_ResultElement_Calculation().Calculate(
+                        w,
+                        t,
+                        RungeKuttaResults))
+                .ToImmutableList());
+        }
+    }
+}
