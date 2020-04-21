3894d6f43ff78ed5e9ca8c59e726e44bb3bfae4d
Justin Britt <britt@uwindsor.ca>
2020-04-21 14:23:07 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/Results/DayVaccinatedIndividuals/IV.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Results.DayVaccinatedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayVaccinatedIndividuals;
+
+    internal interface IV
+    {
+        ImmutableList<IV_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
