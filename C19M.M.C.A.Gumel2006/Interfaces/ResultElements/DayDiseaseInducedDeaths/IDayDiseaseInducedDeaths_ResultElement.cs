d0690acc60298cb25459e35095588f3aeb9788a4
Justin Britt <britt@uwindsor.ca>
2020-04-21 17:23:21 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/ResultElements/DayDiseaseInducedDeaths/IDayDiseaseInducedDeaths_ResultElement.cs
@@ -0,0 +1,21 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IDayDiseaseInducedDeaths_ResultElement
+    {
+        FhirDateTime t_IndexElement { get; }
+
+        /// <summary>
+        /// Gets the number of disease-induced deaths.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
