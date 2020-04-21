8ce916678ae377c5fbd2eecbe9aa570ba30ab38e
Justin Britt <britt@uwindsor.ca>
2020-04-21 08:28:19 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/ResultElements/DaySusceptibleIndividuals/S_ResultElement.cs
@@ -0,0 +1,37 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.ResultElements.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DaySusceptibleIndividuals;
+
+    internal sealed class S_ResultElement : IS_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S_ResultElement(
+            FhirDateTime t_IndexElement,
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public FhirDateTime t_IndexElement { get; }
+
+        public FhirDecimal Value { get; }
+    }
+}
