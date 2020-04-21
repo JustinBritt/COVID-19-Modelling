169be809399bc895122870a437ef73b921f6eb17
Justin Britt <britt@uwindsor.ca>
2020-04-21 17:52:35 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Calculations/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation.cs
@@ -0,0 +1,47 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Calculations.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Results.DayDiseaseInducedDeaths;
+
+    internal sealed class DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation : IDayCumulativeDiseaseInducedDeaths_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IDayCumulativeDiseaseInducedDeaths_ResultElement Calculate(
+            FhirDateTime t_IndexElement,
+            It t,
+            IDayDiseaseInducedDeaths dayDiseaseInducedDeaths)
+        {
+            return new C19M.M.C.A.Gumel2006.Classes.ResultElements.DayCumulativeDiseaseInducedDeaths.DayCumulativeDiseaseInducedDeaths_ResultElement(
+                t_IndexElement,
+                new FhirDecimal(
+                    t.Value
+                    .Where(w => w >= t.StartDate && w <= t_IndexElement)
+                    .Select(w => dayDiseaseInducedDeaths.GetElementAtAsdecimal(w))
+                    .Sum()));
+        }
+    }
+}
