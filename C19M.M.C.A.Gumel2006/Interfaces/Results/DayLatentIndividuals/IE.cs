141d896e6182823e50005e52e062aab7cf66b233
Justin Britt <britt@uwindsor.ca>
2020-04-21 08:41:03 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Interfaces/Results/DayLatentIndividuals/IE.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Gumel2006.Interfaces.Results.DayLatentIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.ResultElements.DayLatentIndividuals;
+
+    internal interface IE
+    {
+        ImmutableList<IE_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
