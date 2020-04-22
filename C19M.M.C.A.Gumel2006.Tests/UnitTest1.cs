1aee003845d8f2d64fd8be3c1f9a4c165252025b
Justin Britt <britt@uwindsor.ca>
2020-04-22 14:23:41 -0400

    Changes related to dates

--- C19M.M.C.A.Gumel2006.Tests/UnitTest1.cs
+++ C19M.M.C.A.Gumel2006.Tests/UnitTest1.cs
@@ -18,8 +18,29 @@
         {
             C19M.D.Gumel2006.Interfaces.IGreaterTorontoArea GreaterTorontoArea = new C19M.D.Gumel2006.Classes.GreaterTorontoArea();
 
+            // Use StartDate, EndDate,and NumberDaysAfterStartDate from Gumel et al. (2004)
+            FhirDateTime startDate = new FhirDateTime(new DateTimeOffset(new DateTime(2003, 2, 23)));
+
+            FhirDateTime endDate = new FhirDateTime(new DateTimeOffset(new DateTime(2003, 9, 1)));
+
+            Func<FhirDateTime, PositiveInt> numberDaysAfterStartDate =
+            (x) =>
+            {
+                if (x.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date >= startDate.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date)
+                {
+                    return new PositiveInt((int)Math.Abs(Math.Round((x.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date - startDate.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date).TotalDays)));
+                }
+                else
+                {
+                    return new PositiveInt(0);
+                }
+            };
+
             // Context
             C19M.M.C.A.Gumel2006.Interfaces.Contexts.IGumel2006_Context context = new C19M.M.C.A.Gumel2006.Classes.Contexts.Gumel2006_Context(
+                endDate,
+                numberDaysAfterStartDate,
+                startDate,
                 GreaterTorontoArea.DiseaseInducedMortalityRate,
                 new Hl7.Fhir.Model.FhirDecimal(6), // TODO: Change
                 new Hl7.Fhir.Model.FhirDecimal(1), // TODO: Change
