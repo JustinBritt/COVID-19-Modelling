2b5075c83504b464206437954625a4093411688f
Justin Britt <britt@uwindsor.ca>
2020-04-22 16:33:59 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Solutions/RescaledDiseaseTransmission_Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Solutions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Solutions;
+
+    internal sealed class RescaledDiseaseTransmission_Solution : IRescaledDiseaseTransmission_Solution
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public RescaledDiseaseTransmission_Solution()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
