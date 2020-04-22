b20e0fb73b13f6247c7e844ff78011cbc448cd1f
Justin Britt <britt@uwindsor.ca>
2020-04-22 14:49:44 -0400

    Added class

---
+++ C19M.M.C.A.Gumel2006/Classes/Calculations/DayCumulativeProbableCases/DayCumulativeProbableCases_Calculation.cs
@@ -0,0 +1,46 @@
+﻿namespace C19M.M.C.A.Gumel2006.Classes.Calculations.DayCumulativeProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Calculations.DayCumulativeProbableCases;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Indices;
+
+    using C19M.M.C.A.Gumel2006.Interfaces.Results.DayCumulativeProbableCases;
+    using C19M.M.C.A.Gumel2006.Interfaces.Results.DayProbableCases;
+
+    internal sealed class DayCumulativeProbableCases_Calculation : IDayCumulativeProbableCases_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeProbableCases_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IDayCumulativeProbableCases Calculate(
+            It t,
+            IDayProbableCases dayProbableCases)
+        {
+            return new C19M.M.C.A.Gumel2006.Classes.Results.DayCumulativeProbableCases.DayCumulativeProbableCases(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Gumel2006.Classes.Calculations.DayCumulativeProbableCases.DayCumulativeProbableCases_ResultElement_Calculation().Calculate(
+                        w,
+                        t,
+                        dayProbableCases))
+                .ToImmutableList());
+        }
+    }
+}
