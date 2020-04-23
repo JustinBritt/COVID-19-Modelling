53c6ee7f48e71549ce507702fe19fd10aaa5e2b8
Justin Britt <britt@uwindsor.ca>
2020-04-23 17:03:43 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Variables/RecoveredIndividuals/R.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Variables.RecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Variables.RecoveredIndividuals;
+
+    internal sealed class R : IR
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R(
+            FhirDecimal initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public FhirDecimal InitialValue { get; }
+    }
+}
