d27df2112265e14a918a4097813e6a4cc24a4aaf
Justin Britt <britt@uwindsor.ca>
2020-04-23 16:55:02 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Variables/QuarantinedIndividuals/Q.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Variables.QuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Variables.QuarantinedIndividuals;
+
+    internal sealed class Q : IQ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Q(
+            FhirDecimal initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public FhirDecimal InitialValue { get; }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Variables/QuarantinedIndividuals/IQ.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Variables.QuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IQ
+    {
+        FhirDecimal InitialValue { get; }
+    }
+}
