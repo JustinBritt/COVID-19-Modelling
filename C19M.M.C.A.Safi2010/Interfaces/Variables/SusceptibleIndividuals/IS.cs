56ce25b621e94daf44a23bc46499c85e257da7fd
Justin Britt <britt@uwindsor.ca>
2020-04-23 16:30:54 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Variables/SusceptibleIndividuals/IS.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Variables.SusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IS
+    {
+        FhirDecimal InitialValue { get; }
+    }
+}
