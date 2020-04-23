297e28909c00b54c5cd4c933c2054d1173907ab9
Justin Britt <britt@uwindsor.ca>
2020-04-23 16:52:54 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Variables/ExposedIndividuals/E.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Variables.ExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Variables.ExposedIndividuals;
+
+    internal sealed class E : IE
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E(
+            FhirDecimal initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public FhirDecimal InitialValue { get; }
+    }
+}
