5c4e166dea379be5a6ef46cb07de5d0119fb45a7
Justin Britt <britt@uwindsor.ca>
2020-04-23 17:01:02 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Variables/HospitalizedIndividuals/H.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Variables.HospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Variables.HospitalizedIndividuals;
+
+    internal sealed class H : IH
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public H(
+            FhirDecimal initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public FhirDecimal InitialValue { get; }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Variables/HospitalizedIndividuals/IH.cs
@@ -0,0 +1,16 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Variables.HospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IH
+    {
+        FhirDecimal InitialValue { get; }
+    }
+}
