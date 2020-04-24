0d2ea5c83cf13e8eab75a4840b9f8a2ab436de10
Justin Britt <britt@uwindsor.ca>
2020-04-24 16:57:45 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/DiseaseInducedDeathRateHospitalizedIndividuals/Iδ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateHospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iδ
+    {
+        /// <summary>
+        /// Gets the disease-induced death rate for hospitalized individuals.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
