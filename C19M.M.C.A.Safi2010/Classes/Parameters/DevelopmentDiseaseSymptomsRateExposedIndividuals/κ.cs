1a9a4cb0aad30850bdff4e93ebdd7639d1465d17
Justin Britt <britt@uwindsor.ca>
2020-04-24 12:35:46 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Parameters/DevelopmentDiseaseSymptomsRateExposedIndividuals/κ.cs
@@ -0,0 +1,35 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Parameters.DevelopmentDiseaseSymptomsRateExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.DevelopmentDiseaseSymptomsRateExposedIndividuals;
+
+    internal sealed class κ : Iκ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public κ(
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the rate at which exposed individuals develop disease symptoms.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/DevelopmentDiseaseSymptomsRateExposedIndividuals/Iκ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.DevelopmentDiseaseSymptomsRateExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iκ
+    {
+        /// <summary>
+        /// Gets the rate at which exposed individuals develop disease symptoms.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
