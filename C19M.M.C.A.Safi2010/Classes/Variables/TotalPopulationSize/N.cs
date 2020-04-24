3c4e5a2878133ce4b040e493fbc85b986540eacd
Justin Britt <britt@uwindsor.ca>
2020-04-24 11:07:39 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Variables/TotalPopulationSize/N.cs
@@ -0,0 +1,32 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Variables.TotalPopulationSize
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Variables.TotalPopulationSize;
+
+    internal sealed class N : IN
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public N(
+            FhirDecimal initialValue)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.InitialValue = initialValue;
+        }
+
+        public FhirDecimal InitialValue { get; }
+    }
+}
