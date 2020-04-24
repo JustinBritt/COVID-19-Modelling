d3293718714599f682f669e144d7b2598c48d60a
Justin Britt <britt@uwindsor.ca>
2020-04-24 11:41:22 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/AcquireInfectionRateSusceptibleIndividuals/Iλ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.AcquireInfectionRateSusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iλ
+    {
+        /// <summary>
+        /// Gets the rate at which susceptible individuals acquire infections.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
