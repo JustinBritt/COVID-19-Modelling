32e9d190fadae06ab56735f007349d5d24dc3dae
Justin Britt <britt@uwindsor.ca>
2020-04-24 12:06:26 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/NaturalDeathRate/Iμ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iμ
+    {
+        /// <summary>
+        /// Gets the natural death rate.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
