2f902f5302b6730119fab1d162b6b3b59cee0083
Justin Britt <britt@uwindsor.ca>
2020-04-24 11:17:20 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Parameters/RecruitmentRateSusceptibleIndividuals/Π.cs
@@ -0,0 +1,35 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Parameters.RecruitmentRateSusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.RecruitmentRateSusceptibleIndividuals;
+
+    internal sealed class Π : IΠ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Π(
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the recruitment rate of susceptible individuals.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
