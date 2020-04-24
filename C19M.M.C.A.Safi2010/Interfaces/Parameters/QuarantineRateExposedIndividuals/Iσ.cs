55faa099e63b6ebc1a5f3378d236795b7268095a
Justin Britt <britt@uwindsor.ca>
2020-04-24 13:51:25 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/QuarantineRateExposedIndividuals/Iσ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.QuarantineRateExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iσ
+    {
+        /// <summary>
+        /// Gets the quarantine rate for exposed individuals;
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
