45f9764f1878e8f98c63f775e6d5cd12d2ebe051
Justin Britt <britt@uwindsor.ca>
2020-04-24 15:46:09 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/RecoveryRateHospitalizedIndividuals/Iγ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.RecoveryRateHospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iγ
+    {
+        /// <summary>
+        /// Gets the recovery rate for hospitalized individuals.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
