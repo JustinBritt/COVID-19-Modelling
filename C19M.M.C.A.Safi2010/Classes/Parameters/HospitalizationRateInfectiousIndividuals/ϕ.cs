8b7770d4e19d0e2755b5fdbb40d0edc7004f9ebd
Justin Britt <britt@uwindsor.ca>
2020-04-24 15:02:54 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Parameters/HospitalizationRateInfectiousIndividuals/ϕ.cs
@@ -0,0 +1,35 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Parameters.HospitalizationRateInfectiousIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.HospitalizationRateInfectiousIndividuals;
+
+    internal sealed class ϕ : Iϕ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public ϕ(
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the hospitalization rate for infectious individuals.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
