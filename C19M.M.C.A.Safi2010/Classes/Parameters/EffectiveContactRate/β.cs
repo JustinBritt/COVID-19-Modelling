121319f005e6b2ed0ddeb10ed97157b02374af62
Justin Britt <britt@uwindsor.ca>
2020-04-24 11:54:09 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Parameters/EffectiveContactRate/β.cs
@@ -0,0 +1,35 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Parameters.EffectiveContactRate
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.EffectiveContactRate;
+
+    internal sealed class β : Iβ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public β(
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the effective contact rate.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
