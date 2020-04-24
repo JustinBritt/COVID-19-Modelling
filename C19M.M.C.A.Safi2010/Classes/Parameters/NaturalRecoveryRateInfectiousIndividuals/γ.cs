8faec824355a5dde935e9b7949175788c593d52b
Justin Britt <britt@uwindsor.ca>
2020-04-24 14:56:29 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Parameters/NaturalRecoveryRateInfectiousIndividuals/γ.cs
@@ -0,0 +1,35 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Parameters.NaturalRecoveryRateInfectiousIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalRecoveryRateInfectiousIndividuals;
+
+    internal sealed class γ : Iγ
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public γ(
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        /// <summary>
+        /// Gets the natural recovery rate for infectious individuals.
+        /// </summary>
+        public FhirDecimal Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Parameters/NaturalRecoveryRateInfectiousIndividuals/Iγ.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalRecoveryRateInfectiousIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface Iγ
+    {
+        /// <summary>
+        /// Gets the natural recovery rate for infectious individuals.
+        /// </summary>
+        FhirDecimal Value { get; }
+    }
+}
