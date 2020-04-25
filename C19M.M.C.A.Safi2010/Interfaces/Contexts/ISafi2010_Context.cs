a1842f8c5579ddcf7ec5a79748c9f73b208feba8
Justin Britt <britt@uwindsor.ca>
2020-04-25 15:12:25 -0400

    Added properties

--- C19M.M.C.A.Safi2010/Interfaces/Contexts/ISafi2010_Context.cs
+++ C19M.M.C.A.Safi2010/Interfaces/Contexts/ISafi2010_Context.cs
@@ -11,5 +11,88 @@
 
     public interface ISafi2010_Context
     {
+        /// <summary>
+        /// Gets the hospitalization rate for quarantined individuals.
+        /// Parameter: α
+        /// </summary>
+        FhirDecimal HospitalizationRateQuarantinedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the effective contact rate.
+        /// Parameter: β
+        /// </summary>
+        FhirDecimal EffectiveContactRate { get; }
+
+        /// <summary>
+        /// Gets the natural recovery rate for infectious individuals.
+        /// Parameter: γ_1
+        /// </summary>
+        FhirDecimal NaturalRecoveryRateInfectiousIndividuals { get; }
+
+        /// <summary>
+        /// Gets the recovery rate for hospitalized individuals.
+        /// Parameter: γ_2
+        /// </summary>
+        FhirDecimal RecoveryRateHospitalizedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the disease-induced death rate for infectious individuals.
+        /// Parameter: δ_1
+        /// </summary>
+        FhirDecimal DiseaseInducedDeathRateInfectiousIndividuals { get; }
+
+        /// <summary>
+        /// Gets the disease-induced death rate for hospitalized individuals.
+        /// Parameter: δ_2
+        /// </summary>
+        FhirDecimal DiseaseInducedDeathRateHospitalizedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the efficacy of isolation or treatment given to hospitalized individuals.
+        /// Parameter: η
+        /// </summary>
+        FhirDecimal EfficacyIsolationTreatmentHospitalizedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the rate at which exposed individuals develop disease symptoms.
+        /// Parameter: κ
+        /// </summary>
+        FhirDecimal DevelopmentDiseaseSymptomsRateExposedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the rate at which susceptible individuals acquire infections.
+        /// Parameter: λ
+        /// </summary>
+        FhirDecimal AcquireInfectionRateSusceptibleIndividuals { get; }
+
+        /// <summary>
+        /// Gets the natural death rate.
+        /// Parameter: μ
+        /// </summary>
+        FhirDecimal NaturalDeathRate { get; }
+
+        /// <summary>
+        /// Gets the recruitment rate of susceptible individuals.
+        /// Parameter: Π
+        /// </summary>
+        FhirDecimal RecruitmentRateSusceptibleIndividuals { get; }
+
+        /// <summary>
+        /// Gets the quarantine rate for exposed individuals.
+        /// Parameter: σ
+        /// </summary>
+        FhirDecimal QuarantineRateExposedIndividuals { get; }
+
+        /// <summary>
+        /// Gets the hospitalization rate for infectious individuals.
+        /// Parameter: ϕ
+        /// </summary>
+        FhirDecimal HospitalizationRateInfectiousIndividuals { get; }
+
+        /// <summary>
+        /// Gets the rate at which recovered individuals lose their infection-acquired immunity.
+        /// Parameter: ψ
+        /// </summary>
+        FhirDecimal InfectionAcquiredImmunityLossRateRecoveredIndividuals { get; }
     }
 }
