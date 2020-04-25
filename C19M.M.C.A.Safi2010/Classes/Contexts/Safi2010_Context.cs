8e6bc8d1c46a88ae1a65c8864004bdd5f718afd1
Justin Britt <britt@uwindsor.ca>
2020-04-25 10:12:54 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Contexts/Safi2010_Context.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Contexts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Contexts;
+
+    public sealed class Safi2010_Context : ISafi2010_Context
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Safi2010_Context()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Contexts/ISafi2010_Context.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Contexts
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public interface ISafi2010_Context
+    {
+    }
+}
