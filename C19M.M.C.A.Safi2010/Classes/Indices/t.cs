eeb618dc81d32b94f64c6aa0f7a63411599632c5
Justin Britt <britt@uwindsor.ca>
2020-04-26 16:31:58 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Indices/t.cs
@@ -0,0 +1,57 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Indices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Indices;
+
+    internal sealed class t : It
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public t(
+            FhirDateTime endDate,
+            Func<FhirDateTime, PositiveInt> numberDaysAfterStartDate,
+            FhirDateTime startDate)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.EndDate = endDate;
+
+            this.NumberDaysAfterStartDate = numberDaysAfterStartDate;
+
+            this.StartDate = startDate;
+
+            ImmutableList<FhirDateTime>.Builder builder = ImmutableList.CreateBuilder<FhirDateTime>();
+
+            for (DateTime i = startDate.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date; i <= endDate.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date; i = i.AddDays(1))
+            {
+                builder.Add(
+                    new FhirDateTime(
+                        new DateTimeOffset(
+                            i)));
+            }
+
+            this.Value = builder.ToImmutableList();
+        }
+
+        public FhirDateTime EndDate { get; }
+
+        public Func<FhirDateTime, PositiveInt> NumberDaysAfterStartDate { get; }
+
+        public FhirDateTime StartDate { get; }
+
+        public ImmutableList<FhirDateTime> Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Indices/It.cs
@@ -0,0 +1,23 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Indices
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface It
+    {
+        FhirDateTime EndDate { get; }
+
+        Func<FhirDateTime, PositiveInt> NumberDaysAfterStartDate { get; }
+
+        FhirDateTime StartDate { get; }
+
+        ImmutableList<FhirDateTime> Value { get; }
+    }
+}
