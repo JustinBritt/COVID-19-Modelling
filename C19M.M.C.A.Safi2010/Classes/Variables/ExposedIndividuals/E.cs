9831717e8f281e6622d7f6e0a3206d2af6d852af
Justin Britt <britt@uwindsor.ca>
2020-04-26 18:49:03 -0400

    Added method

--- C19M.M.C.A.Safi2010/Classes/Variables/ExposedIndividuals/E.cs
+++ C19M.M.C.A.Safi2010/Classes/Variables/ExposedIndividuals/E.cs
@@ -12,6 +12,13 @@
     // Hl7
     using Hl7.Fhir.Model;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.AcquireInfectionRateSusceptibleIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.DevelopmentDiseaseSymptomsRateExposedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.EffectiveContactRate;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.EfficacyIsolationTreatmentHospitalizedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.QuarantineRateExposedIndividuals;
+
     using C19M.M.C.A.Safi2010.Interfaces.Variables.ExposedIndividuals;
 
     internal sealed class E : IE
@@ -28,5 +35,33 @@
         }
 
         public FhirDecimal InitialValue { get; }
+
+        public decimal GetdEdt(
+            Iβ β,
+            Iη η,
+            Iκ κ,
+            Iλ λ,
+            Iμ μ,
+            Iσ σ,
+            double E,
+            double H,
+            double I,
+            double N,
+            double S)
+        {
+            return
+                λ.GetValue(
+                    β,
+                    η,
+                    H,
+                    I,
+                    N).Value.Value 
+                    * 
+                    (decimal)S
+                    -
+                    (κ.Value.Value.Value + σ.Value.Value.Value + μ.Value.Value.Value) 
+                    * 
+                    (decimal)E;
+        }
     }
 }
--- C19M.M.C.A.Safi2010/Interfaces/Variables/ExposedIndividuals/IE.cs
+++ C19M.M.C.A.Safi2010/Interfaces/Variables/ExposedIndividuals/IE.cs
@@ -9,8 +9,28 @@
     // Hl7
     using Hl7.Fhir.Model;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.AcquireInfectionRateSusceptibleIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.DevelopmentDiseaseSymptomsRateExposedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.EffectiveContactRate;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.EfficacyIsolationTreatmentHospitalizedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.QuarantineRateExposedIndividuals;
+
     internal interface IE
     {
         FhirDecimal InitialValue { get; }
+
+        decimal GetdEdt(
+            Iβ β,
+            Iη η,
+            Iκ κ,
+            Iλ λ,
+            Iμ μ,
+            Iσ σ,
+            double E,
+            double H,
+            double I,
+            double N,
+            double S);
     }
 }
