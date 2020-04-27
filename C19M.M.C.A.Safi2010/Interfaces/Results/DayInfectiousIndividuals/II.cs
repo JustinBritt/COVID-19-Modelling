fab3c0ffad587f05c1935ae2d143bfb362e187e5
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:20:05 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayInfectiousIndividuals/II.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayInfectiousIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayInfectiousIndividuals;
+
+    internal interface II
+    {
+        ImmutableList<II_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+           FhirDateTime t_IndexElement);
+    }
+}
