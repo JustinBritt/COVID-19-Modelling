a8681e6e222d566ad159f812afa1bb6d0236dd79
Justin Britt <britt@uwindsor.ca>
2020-04-27 14:14:06 -0400

    Added properties

--- C19M.M.C.A.Safi2010/Interfaces/Models/IEquations2_Model.cs
+++ C19M.M.C.A.Safi2010/Interfaces/Models/IEquations2_Model.cs
@@ -8,5 +8,48 @@
 
     internal interface IEquations2_Model
     {
+        It t { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.HospitalizationRateQuarantinedIndividuals.Iα α { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.EffectiveContactRate.Iβ β { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalRecoveryRateInfectiousIndividuals.Iγ γ_1 { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.RecoveryRateHospitalizedIndividuals.Iγ γ_2 { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateInfectiousIndividuals.Iδ δ_1 { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateHospitalizedIndividuals.Iδ δ_2 { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.EfficacyIsolationTreatmentHospitalizedIndividuals.Iη η { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.DevelopmentDiseaseSymptomsRateExposedIndividuals.Iκ κ { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.AcquireInfectionRateSusceptibleIndividuals.Iλ λ { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate.Iμ μ { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.RecruitmentRateSusceptibleIndividuals.IΠ Π { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.QuarantineRateExposedIndividuals.Iσ σ { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.HospitalizationRateInfectiousIndividuals.Iϕ ϕ { get; }
+
+        C19M.M.C.A.Safi2010.Interfaces.Parameters.InfectionAcquiredImmunityLossRateRecoveredIndividuals.Iψ ψ { get; }
+
+        IE E { get; }
+
+        IH H { get; }
+
+        II I { get; }
+
+        IN N { get; }
+
+        IQ Q { get; }
+
+        IR R { get; }
+
+        IS S { get; }
     }
 }
