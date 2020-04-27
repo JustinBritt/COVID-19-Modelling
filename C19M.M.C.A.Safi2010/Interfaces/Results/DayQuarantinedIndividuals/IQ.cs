4bb0b3d5e03b3c4e81cd3adf85c536e0f24c070e
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:23:47 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayQuarantinedIndividuals/IQ.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayQuarantinedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IQ
+    {
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
