d2ce9fef54225ddeb6b9468aff1fcfaf90a01298
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:30:20 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Results/DaySusceptibleIndividuals/S.cs
@@ -0,0 +1,44 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Results.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DaySusceptibleIndividuals;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DaySusceptibleIndividuals;
+
+    internal sealed class S : IS
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S(
+            ImmutableList<IS_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IS_ResultElement> Value { get; }
+
+        public decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement)
+        {
+            return this.Value
+                .Where(x => x.t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date == t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date)
+                .Select(x => x.Value.Value.Value)
+                .SingleOrDefault();
+        }
+    }
+}
