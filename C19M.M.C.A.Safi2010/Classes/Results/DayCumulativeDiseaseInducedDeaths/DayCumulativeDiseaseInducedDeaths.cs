6961f74c484e6bfa8543f4f62c1f9ac57ec4e182
Justin Britt <britt@uwindsor.ca>
2020-04-27 17:38:00 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Results/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths.cs
@@ -0,0 +1,35 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Results.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayCumulativeDiseaseInducedDeaths;
+
+    internal sealed class DayCumulativeDiseaseInducedDeaths : IDayCumulativeDiseaseInducedDeaths
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeDiseaseInducedDeaths(
+            ImmutableList<IDayCumulativeDiseaseInducedDeaths_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IDayCumulativeDiseaseInducedDeaths_ResultElement> Value { get; }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayCumulativeDiseaseInducedDeaths/IDayCumulativeDiseaseInducedDeaths.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayCumulativeDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    internal interface IDayCumulativeDiseaseInducedDeaths
+    {
+        ImmutableList<IDayCumulativeDiseaseInducedDeaths_ResultElement> Value { get; }
+    }
+}
