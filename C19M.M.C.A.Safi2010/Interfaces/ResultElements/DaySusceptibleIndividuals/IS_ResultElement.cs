153baec68c3c2cae7aa8c006651eb5ad7131d00c
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:06:10 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/ResultElements/DaySusceptibleIndividuals/IS_ResultElement.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.ResultElements.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IS_ResultElement
+    {
+        FhirDateTime t_IndexElement { get; }
+
+        FhirDecimal Value { get; }
+    }
+}
