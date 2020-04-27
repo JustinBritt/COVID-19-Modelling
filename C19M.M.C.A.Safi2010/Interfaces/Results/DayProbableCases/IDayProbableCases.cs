b0163acc4be749b3fd486315fb0fcfcb61bcf303
Justin Britt <britt@uwindsor.ca>
2020-04-27 17:28:50 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayProbableCases/IDayProbableCases.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayProbableCases;
+
+    internal interface IDayProbableCases
+    {
+        ImmutableList<IDayProbableCases_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
