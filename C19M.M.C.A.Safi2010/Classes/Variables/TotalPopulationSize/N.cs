ee3e10bae5c305807f55a739ea52457961bf864c
Justin Britt <britt@uwindsor.ca>
2020-04-27 13:25:49 -0400

    Added method

--- C19M.M.C.A.Safi2010/Classes/Variables/TotalPopulationSize/N.cs
+++ C19M.M.C.A.Safi2010/Classes/Variables/TotalPopulationSize/N.cs
@@ -12,6 +12,9 @@
     // Hl7
     using Hl7.Fhir.Model;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.RecruitmentRateSusceptibleIndividuals;
+
     using C19M.M.C.A.Safi2010.Interfaces.Variables.TotalPopulationSize;
 
     internal sealed class N : IN
@@ -28,5 +31,24 @@
         }
 
         public FhirDecimal InitialValue { get; }
+
+        public decimal GetdNdt(
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateInfectiousIndividuals.Iδ δ_1,
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateHospitalizedIndividuals.Iδ δ_2,
+            Iμ μ,
+            IΠ Π,
+            double H,
+            double I,
+            double N)
+        {
+            return
+                Π.Value.Value.Value
+                -
+                μ.Value.Value.Value * (decimal)N
+                -
+                (δ_1.Value.Value.Value * (decimal)I 
+                +
+                δ_2.Value.Value.Value * (decimal)H);
+        }
     }
 }
--- C19M.M.C.A.Safi2010/Interfaces/Variables/TotalPopulationSize/IN.cs
+++ C19M.M.C.A.Safi2010/Interfaces/Variables/TotalPopulationSize/IN.cs
@@ -9,8 +9,20 @@
     // Hl7
     using Hl7.Fhir.Model;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.RecruitmentRateSusceptibleIndividuals;
+
     internal interface IN
     {
         FhirDecimal InitialValue { get; }
+
+        decimal GetdNdt(
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateInfectiousIndividuals.Iδ δ_1,
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateHospitalizedIndividuals.Iδ δ_2,
+            Iμ μ,
+            IΠ Π,
+            double H,
+            double I,
+            double N);
     }
 }
