0a84792117bfa0dac9c52efa6df0ac178ef35d54
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:16:04 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayHospitalizedIndividuals/IH.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayHospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayHospitalizedIndividuals;
+
+    internal interface IH
+    {
+        ImmutableList<IH_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
