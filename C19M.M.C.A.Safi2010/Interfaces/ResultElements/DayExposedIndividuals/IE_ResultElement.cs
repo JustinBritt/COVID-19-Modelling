f09a0f38ae80611bf5eaeb72bb0d5c01577c7b01
Justin Britt <britt@uwindsor.ca>
2020-04-27 14:44:43 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/ResultElements/DayExposedIndividuals/IE_ResultElement.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IE_ResultElement
+    {
+        FhirDateTime t_IndexElement { get; }
+
+        FhirDecimal Value { get; }
+    }
+}
