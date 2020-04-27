b3296a88c184e6ecc9b62adce49c88068b502d54
Justin Britt <britt@uwindsor.ca>
2020-04-27 17:32:23 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Results/DayDiseaseInducedDeaths/DayDiseaseInducedDeaths.cs
@@ -0,0 +1,44 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Results.DayDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayDiseaseInducedDeaths;
+
+    internal sealed class DayDiseaseInducedDeaths : IDayDiseaseInducedDeaths
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayDiseaseInducedDeaths(
+            ImmutableList<IDayDiseaseInducedDeaths_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IDayDiseaseInducedDeaths_ResultElement> Value { get; }
+
+        public decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement)
+        {
+            return this.Value
+                .Where(x => x.t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date == t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date)
+                .Select(x => x.Value.Value.Value)
+                .SingleOrDefault();
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayDiseaseInducedDeaths/IDayDiseaseInducedDeaths.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayDiseaseInducedDeaths;
+
+    internal interface IDayDiseaseInducedDeaths
+    {
+        ImmutableList<IDayDiseaseInducedDeaths_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
