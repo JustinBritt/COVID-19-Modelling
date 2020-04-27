438dc67213409eb37faec4b7f4c23bb4ca849be3
Justin Britt <britt@uwindsor.ca>
2020-04-27 13:58:25 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Solutions/Equations2_Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Solutions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Solutions;
+
+    internal sealed class Equations2_Solution : IEquations2_Solution
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public Equations2_Solution()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
