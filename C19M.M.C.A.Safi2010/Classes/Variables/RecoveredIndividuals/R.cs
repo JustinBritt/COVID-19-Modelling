8bc0655ab6e6e5203a3cde2cfbc04b2bca356df3
Justin Britt <britt@uwindsor.ca>
2020-04-27 13:38:33 -0400

    Added method

--- C19M.M.C.A.Safi2010/Classes/Variables/RecoveredIndividuals/R.cs
+++ C19M.M.C.A.Safi2010/Classes/Variables/RecoveredIndividuals/R.cs
@@ -12,6 +12,9 @@
     // Hl7
     using Hl7.Fhir.Model;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.InfectionAcquiredImmunityLossRateRecoveredIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate;
+
     using C19M.M.C.A.Safi2010.Interfaces.Variables.RecoveredIndividuals;
 
     internal sealed class R : IR
@@ -28,5 +31,28 @@
         }
 
         public FhirDecimal InitialValue { get; }
+
+        public decimal GetdRdt(
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalRecoveryRateInfectiousIndividuals.Iγ γ_1,
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.RecoveryRateHospitalizedIndividuals.Iγ γ_2,
+            Iμ μ,
+            Iψ ψ,
+            double H,
+            double I,
+            double R)
+        {
+            return
+                γ_1.Value.Value.Value
+                *
+                (decimal)I
+                +
+                γ_2.Value.Value.Value
+                *
+                (decimal)H
+                -
+                (ψ.Value.Value.Value + μ.Value.Value.Value)
+                *
+                (decimal)R;
+        }
     }
 }
--- C19M.M.C.A.Safi2010/Interfaces/Variables/RecoveredIndividuals/IR.cs
+++ C19M.M.C.A.Safi2010/Interfaces/Variables/RecoveredIndividuals/IR.cs
@@ -9,8 +9,20 @@
     // Hl7
     using Hl7.Fhir.Model;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.InfectionAcquiredImmunityLossRateRecoveredIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalDeathRate;
+
     internal interface IR
     {
         FhirDecimal InitialValue { get; }
+
+        decimal GetdRdt(
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.NaturalRecoveryRateInfectiousIndividuals.Iγ γ_1,
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.RecoveryRateHospitalizedIndividuals.Iγ γ_2,
+            Iμ μ,
+            Iψ ψ,
+            double H,
+            double I,
+            double R);
     }
 }
