4f658f766d228ceef0c2f5529e57596854bf3070
Justin Britt <britt@uwindsor.ca>
2020-04-27 14:56:58 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/ResultElements/DayInfectiousIndividuals/I_ResultElement.cs
@@ -0,0 +1,37 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.ResultElements.DayInfectiousIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayInfectiousIndividuals;
+
+    internal sealed class I_ResultElement : II_ResultElement
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public I_ResultElement(
+            FhirDateTime t_IndexElement,
+            FhirDecimal value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.t_IndexElement = t_IndexElement;
+
+            this.Value = value;
+        }
+
+        public FhirDateTime t_IndexElement { get; }
+
+        public FhirDecimal Value { get; }
+    }
+}
