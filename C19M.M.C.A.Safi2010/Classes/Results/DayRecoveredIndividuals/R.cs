a85c0b0b18a949b5e16ce7b451f0c8ddccab278d
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:26:27 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Results/DayRecoveredIndividuals/R.cs
@@ -0,0 +1,44 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Results.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayRecoveredIndividuals;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayRecoveredIndividuals;
+
+    internal sealed class R : IR
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R(
+            ImmutableList<IR_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IR_ResultElement> Value { get; }
+
+        public decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement)
+        {
+            return this.Value
+                .Where(x => x.t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date == t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date)
+                .Select(x => x.Value.Value.Value)
+                .SingleOrDefault();
+        }
+    }
+}
