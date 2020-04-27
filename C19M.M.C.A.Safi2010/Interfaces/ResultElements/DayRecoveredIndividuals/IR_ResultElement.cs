78150a177328c1f63912e3e2ee4ad6609fa214a9
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:03:45 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/ResultElements/DayRecoveredIndividuals/IR_ResultElement.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    internal interface IR_ResultElement
+    {
+        FhirDateTime t_IndexElement { get; }
+
+        FhirDecimal Value { get; }
+    }
+}
