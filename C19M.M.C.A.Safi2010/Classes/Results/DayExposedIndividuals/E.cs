49e41f8aba2d697660c098360c7a92cc390cd092
Justin Britt <britt@uwindsor.ca>
2020-04-27 15:12:40 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Results/DayExposedIndividuals/E.cs
@@ -0,0 +1,44 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Results.DayExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayExposedIndividuals;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayExposedIndividuals;
+
+    internal sealed class E : IE
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E(
+            ImmutableList<IE_ResultElement> value)
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+            this.Value = value;
+        }
+
+        public ImmutableList<IE_ResultElement> Value { get; }
+
+        public decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement)
+        {
+            return this.Value
+                .Where(x => x.t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date == t_IndexElement.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date)
+                .Select(x => x.Value.Value.Value)
+                .SingleOrDefault();
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Results/DayExposedIndividuals/IE.cs
@@ -0,0 +1,22 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Results.DayExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayExposedIndividuals;
+
+    internal interface IE
+    {
+        ImmutableList<IE_ResultElement> Value { get; }
+
+        decimal GetElementAtAsdecimal(
+            FhirDateTime t_IndexElement);
+    }
+}
