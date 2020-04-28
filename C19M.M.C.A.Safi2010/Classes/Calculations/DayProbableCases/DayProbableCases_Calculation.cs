0fdeb42a6d06536c76c9472744ee00488aae570a
Justin Britt <britt@uwindsor.ca>
2020-04-28 11:48:12 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayProbableCases/DayProbableCases_Calculation.cs
@@ -0,0 +1,54 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayProbableCases;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Indices;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayHospitalizedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayInfectiousIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayProbableCases;
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayQuarantinedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayRecoveredIndividuals;
+
+    internal sealed class DayProbableCases_Calculation : IDayProbableCases_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayProbableCases_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IDayProbableCases Calculate(
+            It t,
+            IH H,
+            II I,
+            IQ Q,
+            IR R)
+        {
+            return new C19M.M.C.A.Safi2010.Classes.Results.DayProbableCases.DayProbableCases(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Safi2010.Classes.Calculations.DayProbableCases.DayProbableCases_ResultElement_Calculation().Calculate(
+                        w,
+                        H,
+                        I,
+                        Q,
+                        R))
+                .ToImmutableList());
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Calculations/DayProbableCases/IDayProbableCases_Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Calculations.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IDayProbableCases_Calculation
+    {
+    }
+}
