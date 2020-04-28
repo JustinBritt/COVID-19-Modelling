c30ab6d68894e9d61ab841947e350bbd400c85f7
Justin Britt <britt@uwindsor.ca>
2020-04-28 15:30:30 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayExposedIndividuals/E_ResultElement_Calculation.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayExposedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayExposedIndividuals;
+
+    internal sealed class E_ResultElement_Calculation : IE_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public E_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
