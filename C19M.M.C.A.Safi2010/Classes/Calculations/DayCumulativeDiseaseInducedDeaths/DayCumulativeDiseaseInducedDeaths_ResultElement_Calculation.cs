93bcbfc4d64bcc96f91500c63f8e45de46d38aa6
Justin Britt <britt@uwindsor.ca>
2020-04-28 13:37:08 -0400

    Added method

--- C19M.M.C.A.Safi2010/Classes/Calculations/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation.cs
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayCumulativeDiseaseInducedDeaths/DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation.cs
@@ -14,6 +14,12 @@
 
     using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayCumulativeDiseaseInducedDeaths;
 
+    using C19M.M.C.A.Safi2010.Interfaces.Indices;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayCumulativeDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayDiseaseInducedDeaths;
+
     internal sealed class DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation : IDayCumulativeDiseaseInducedDeaths_ResultElement_Calculation
     {
         private ILog Log { get; }
@@ -22,6 +28,20 @@
         public DayCumulativeDiseaseInducedDeaths_ResultElement_Calculation()
         {
             this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IDayCumulativeDiseaseInducedDeaths_ResultElement Calculate(
+            FhirDateTime t_IndexElement,
+            It t,
+            IDayDiseaseInducedDeaths dayDiseaseInducedDeaths)
+        {
+            return new C19M.M.C.A.Safi2010.Classes.ResultElements.DayCumulativeDiseaseInducedDeaths.DayCumulativeDiseaseInducedDeaths_ResultElement(
+                t_IndexElement,
+                new FhirDecimal(
+                    t.Value
+                    .Where(w => w >= t.StartDate && w <= t_IndexElement)
+                    .Select(w => dayDiseaseInducedDeaths.GetElementAtAsdecimal(w))
+                    .Sum()));
         }
     }
 }
