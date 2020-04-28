a6fdb5746c1f21246e98ea0d0ba183c50ac56f57
Justin Britt <britt@uwindsor.ca>
2020-04-28 13:09:47 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayDiseaseInducedDeaths/DayDiseaseInducedDeaths_Calculation.cs
@@ -0,0 +1,28 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayDiseaseInducedDeaths;
+
+    internal sealed class DayDiseaseInducedDeaths_Calculation : IDayDiseaseInducedDeaths_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayDiseaseInducedDeaths_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Calculations/DayDiseaseInducedDeaths/IDayDiseaseInducedDeaths_Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Calculations.DayDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IDayDiseaseInducedDeaths_Calculation
+    {
+    }
+}
