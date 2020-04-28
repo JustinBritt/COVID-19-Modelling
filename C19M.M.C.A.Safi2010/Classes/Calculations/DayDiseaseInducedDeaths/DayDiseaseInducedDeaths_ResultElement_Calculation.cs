88b3c9c8f8a66c7c27e356dd93b28f76b7abeb07
Justin Britt <britt@uwindsor.ca>
2020-04-28 12:50:08 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayDiseaseInducedDeaths/DayDiseaseInducedDeaths_ResultElement_Calculation.cs
@@ -0,0 +1,27 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayDiseaseInducedDeaths
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayDiseaseInducedDeaths;
+
+    internal sealed class DayDiseaseInducedDeaths_ResultElement_Calculation : IDayDiseaseInducedDeaths_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayDiseaseInducedDeaths_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
