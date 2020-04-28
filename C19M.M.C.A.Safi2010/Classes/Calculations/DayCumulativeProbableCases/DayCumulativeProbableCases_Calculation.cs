662ced949164080208eb1e2b393b1676a7a2eabe
Justin Britt <britt@uwindsor.ca>
2020-04-28 12:01:32 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayCumulativeProbableCases/DayCumulativeProbableCases_Calculation.cs
@@ -0,0 +1,28 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayCumulativeProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayCumulativeProbableCases;
+
+    internal sealed class DayCumulativeProbableCases_Calculation : IDayCumulativeProbableCases_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayCumulativeProbableCases_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Calculations/DayCumulativeProbableCases/IDayCumulativeProbableCases_Calculation.cs
@@ -0,0 +1,13 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Calculations.DayCumulativeProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IDayCumulativeProbableCases_Calculation
+    {
+    }
+}
