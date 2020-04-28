75615d5a4c19d2ac2e06f7b365793a6affda3b55
Justin Britt <britt@uwindsor.ca>
2020-04-28 13:07:44 -0400

    Added method

--- C19M.M.C.A.Safi2010/Classes/Calculations/DayDiseaseInducedDeaths/DayDiseaseInducedDeaths_ResultElement_Calculation.cs
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayDiseaseInducedDeaths/DayDiseaseInducedDeaths_ResultElement_Calculation.cs
@@ -14,6 +14,11 @@
 
     using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayDiseaseInducedDeaths;
 
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayDiseaseInducedDeaths;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayHospitalizedIndividuals;
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayInfectiousIndividuals;
+
     internal sealed class DayDiseaseInducedDeaths_ResultElement_Calculation : IDayDiseaseInducedDeaths_ResultElement_Calculation
     {
         private ILog Log { get; }
@@ -22,6 +27,27 @@
         public DayDiseaseInducedDeaths_ResultElement_Calculation()
         {
             this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IDayDiseaseInducedDeaths_ResultElement Calculate(
+            FhirDateTime t_IndexElement,
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateInfectiousIndividuals.Iδ δ_1,
+            C19M.M.C.A.Safi2010.Interfaces.Parameters.DiseaseInducedDeathRateHospitalizedIndividuals.Iδ δ_2,
+            IH H,
+            II I)
+        {
+            return new C19M.M.C.A.Safi2010.Classes.ResultElements.DayDiseaseInducedDeaths.DayDiseaseInducedDeaths_ResultElement(
+                t_IndexElement,
+                new FhirDecimal(
+                    δ_1.Value.Value.Value
+                    *
+                    I.GetElementAtAsdecimal(
+                        t_IndexElement)
+                    +
+                    δ_2.Value.Value.Value
+                    *
+                    H.GetElementAtAsdecimal(
+                        t_IndexElement)));
         }
     }
 }
