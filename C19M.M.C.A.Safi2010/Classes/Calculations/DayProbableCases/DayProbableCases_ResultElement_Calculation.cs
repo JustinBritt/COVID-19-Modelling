9c335aaee70fc3e44abea24faa6adb76320516a3
Justin Britt <britt@uwindsor.ca>
2020-04-28 11:27:11 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayProbableCases/DayProbableCases_ResultElement_Calculation.cs
@@ -0,0 +1,29 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayProbableCases;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayProbableCases;
+
+    internal sealed class DayProbableCases_ResultElement_Calculation : IDayProbableCases_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public DayProbableCases_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Calculations/DayProbableCases/IDayProbableCases_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Calculations.DayProbableCases
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IDayProbableCases_ResultElement_Calculation
+    {
+    }
+}
