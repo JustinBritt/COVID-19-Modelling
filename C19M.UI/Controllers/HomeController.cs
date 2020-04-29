51ac645b080452bd613e75557c7908251d55e012
Justin Britt <britt@uwindsor.ca>
2020-04-29 14:58:13 -0400

    Added method

--- C19M.UI/Controllers/HomeController.cs
+++ C19M.UI/Controllers/HomeController.cs
@@ -35,6 +35,68 @@ namespace C19M.UI.Controllers
             return View();
         }
 
+        public ActionResult DisplayChart_Gumel2004_HongKong_DayCumulativeDiseaseInducedDeaths()
+        {
+            ViewBag.Mesage = "";
+
+            C19M.D.Gumel2004.Interfaces.IHongKong HK = new C19M.D.Gumel2004.Classes.HongKong();
+
+            // Context
+            C19M.M.C.A.Gumel2004.Interfaces.Contexts.IGumel2004_Context context = new C19M.M.C.A.Gumel2004.Classes.Contexts.Gumel2004_Context(
+                HK.EndDate,
+                HK.NumberDaysAfterStartDate,
+                HK.StartDate,
+                HK.DiseaseInducedDeathRateSymptomaticIndividuals,
+                HK.DiseaseInducedDeathRateIsolatedIndividuals,
+                HK.InitialValueAsymptomaticIndividuals,
+                HK.InitialValueSymptomaticIndividuals,
+                HK.InitialValueIsolatedIndividuals,
+                HK.RecruitmentRateAsymptomaticIndividuals,
+                HK.InitialValueQuarantinedIndividuals,
+                HK.InitialValueRecoveredIndividuals,
+                null, // GTA.BasicReproductionNumber,
+                null, // GTA.ControlReproductionNumber,
+                HK.InitialValueSusceptibleIndividuals,
+                HK.BasicTransmissionCoefficient,
+                HK.QuarantineRateAsymptomaticIndividuals,
+                HK.IsolationRateSymptomaticIndividuals,
+                HK.TransmissionCoefficientModificationFactorAsymptomaticIndividuals,
+                HK.TransmissionCoefficientModificationFactorIsolatedIndividuals,
+                HK.TransmissionCoefficientModificationFactorQuarantinedIndividuals,
+                HK.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals,
+                HK.DevelopmentClinicalSymptomsRateQuarantinedIndividuals,
+                HK.NaturalDeathRate,
+                HK.NetInflowRateSusceptibleIndividuals,
+                HK.RecoveryRateSymptomaticIndividuals,
+                HK.RecoveryRateIsolatedIndividuals);
+
+            C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
+                context);
+
+            export.Solve();
+
+            ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> dayCumulativeDiseaseInducedDeaths = export.DayCumulativeDiseaseInducedDeaths;
+
+            // https://stackoverflow.com/a/9345910
+            byte[] chart = new Chart(
+                width: 1000,
+                height: 300,
+                theme: ChartTheme.Vanilla)
+                .AddSeries(
+                chartType: "line",
+                xValue: dayCumulativeDiseaseInducedDeaths.Select(w => w.Item1.ToPartialDateTime().Value.ToUniversalTime().Date.Date).ToArray(),
+                yValues: dayCumulativeDiseaseInducedDeaths.Select(w => w.Item2.Value.Value).ToArray())
+                .SetXAxis("Day", (double)HK.NumberDaysAfterStartDate.Invoke(HK.StartDate).Value.Value, (double)HK.NumberDaysAfterStartDate.Invoke(HK.EndDate).Value.Value)
+                .SetYAxis("Cumulative Disease Induced Deaths")
+                .ToWebImage()
+                .GetBytes("image/png");
+
+            // Return byte array as png.
+            return File(
+                chart,
+                "image/png");
+        }
+
         public ActionResult DisplayChart_Gumel2004_HongKong_DayCumulativeProbableCases()
         {
             ViewBag.Mesage = "";
