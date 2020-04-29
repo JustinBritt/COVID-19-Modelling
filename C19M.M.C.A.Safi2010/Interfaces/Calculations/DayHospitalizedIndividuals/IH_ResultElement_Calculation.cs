60a310283f8732d33bcbc004b9163ad783342aa5
Justin Britt <britt@uwindsor.ca>
2020-04-28 20:07:32 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Interfaces/Calculations/DayHospitalizedIndividuals/IH_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Calculations.DayHospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IH_ResultElement_Calculation
+    {
+    }
+}
