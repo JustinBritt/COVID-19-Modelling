46b48208b90b1cd2971adbfbc9ad0874c8dc5ab2
Justin Britt <britt@uwindsor.ca>
2020-04-29 11:56:02 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayRecoveredIndividuals/R_ResultElement_Calculation.cs
@@ -0,0 +1,42 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayRecoveredIndividuals;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Indices;
+
+    using C19M.M.C.A.Safi2010.Interfaces.ResultElements.DayRecoveredIndividuals;
+
+    internal sealed class R_ResultElement_Calculation : IR_ResultElement_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public R_ResultElement_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IR_ResultElement Calculate(
+            FhirDateTime t_IndexElement,
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Safi2010.Classes.ResultElements.DayRecoveredIndividuals.R_ResultElement(
+                t_IndexElement,
+                new FhirDecimal(
+                    (decimal)RungeKuttaResults[t.NumberDaysAfterStartDate.Invoke(t_IndexElement).Value.Value][4]));
+        }
+    }
+}
---
+++ C19M.M.C.A.Safi2010/Interfaces/Calculations/DayRecoveredIndividuals/IR_ResultElement_Calculation.cs
@@ -0,0 +1,12 @@
+﻿namespace C19M.M.C.A.Safi2010.Interfaces.Calculations.DayRecoveredIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal interface IR_ResultElement_Calculation
+    {
+    }
+}
