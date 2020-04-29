eae828f16ba098378ba33b3206a48465e4c25742
Justin Britt <britt@uwindsor.ca>
2020-04-29 10:23:47 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DayHospitalizedIndividuals/H_Calculation.cs
@@ -0,0 +1,46 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DayHospitalizedIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DayHospitalizedIndividuals;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Indices;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DayHospitalizedIndividuals;
+
+    internal sealed class H_Calculation : IH_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public H_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IH Calculate(
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Safi2010.Classes.Results.DayHospitalizedIndividuals.H(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Safi2010.Classes.Calculations.DayHospitalizedIndividuals.H_ResultElement_Calculation().Calculate(
+                        w,
+                        t,
+                        RungeKuttaResults))
+                .ToImmutableList());
+        }
+    }
+}
