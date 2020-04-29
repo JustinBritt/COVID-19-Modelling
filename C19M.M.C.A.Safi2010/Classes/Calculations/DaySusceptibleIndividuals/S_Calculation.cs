9ed9c962d624d6ccb1acab31212c012cd036d2f2
Justin Britt <britt@uwindsor.ca>
2020-04-29 12:06:00 -0400

    Added class

---
+++ C19M.M.C.A.Safi2010/Classes/Calculations/DaySusceptibleIndividuals/S_Calculation.cs
@@ -0,0 +1,45 @@
+﻿namespace C19M.M.C.A.Safi2010.Classes.Calculations.DaySusceptibleIndividuals
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Logging
+    using log4net;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Calculations.DaySusceptibleIndividuals;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Indices;
+
+    using C19M.M.C.A.Safi2010.Interfaces.Results.DaySusceptibleIndividuals;
+
+    internal sealed class S_Calculation : IS_Calculation
+    {
+        private ILog Log { get; }
+
+        // Constructor
+        public S_Calculation()
+        {
+            this.Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        }
+
+        public IS Calculate(
+            It t,
+            MathNet.Numerics.LinearAlgebra.Vector<double>[] RungeKuttaResults)
+        {
+            return new C19M.M.C.A.Safi2010.Classes.Results.DaySusceptibleIndividuals.S(
+                t.Value.Select(
+                    w => new C19M.M.C.A.Safi2010.Classes.Calculations.DaySusceptibleIndividuals.S_ResultElement_Calculation().Calculate(
+                        w,
+                        t,
+                        RungeKuttaResults))
+                .ToImmutableList());
+        }
+    }
+}
