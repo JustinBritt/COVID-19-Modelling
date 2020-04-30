6e8ed097bf3159d3a3544b12013c44577cc45414
Justin Britt <britt@uwindsor.ca>
2020-04-30 00:34:44 -0400

    Added class

---
+++ C19M.UI/Models/GraphData_Model.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.UI.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public class GraphData_Model
+    {
+        public GraphData_Model()
+        {
+        }
+
+        public string x { get; set; }
+
+        public decimal y { get; set; }
+    }
+}
