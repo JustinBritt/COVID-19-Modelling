1ac96c3363e4ec943d2136dfff83582c971fe7ab
Justin Britt <britt@uwindsor.ca>
2020-04-30 10:03:41 -0400

    Moved code

--- C19M.UI/Controllers/HomeController.cs
+++ C19M.UI/Controllers/HomeController.cs
@@ -22,48 +22,6 @@ namespace C19M.UI.Controllers
             // https://forums.asp.net/t/2125582.aspx?Pass+c+array+to+javascript+array+in+razor+file+
             ViewData["Labels"] = new string[] { "2010", "2011", "2012", "2013" };
 
-            C19M.D.Gumel2004.Interfaces.IHongKong HK = new C19M.D.Gumel2004.Classes.HongKong();
-
-            // Context
-            C19M.M.C.A.Gumel2004.Interfaces.Contexts.IGumel2004_Context context = new C19M.M.C.A.Gumel2004.Classes.Contexts.Gumel2004_Context(
-                HK.EndDate,
-                HK.NumberDaysAfterStartDate,
-                HK.StartDate,
-                HK.DiseaseInducedDeathRateSymptomaticIndividuals,
-                HK.DiseaseInducedDeathRateIsolatedIndividuals,
-                HK.InitialValueAsymptomaticIndividuals,
-                HK.InitialValueSymptomaticIndividuals,
-                HK.InitialValueIsolatedIndividuals,
-                HK.RecruitmentRateAsymptomaticIndividuals,
-                HK.InitialValueQuarantinedIndividuals,
-                HK.InitialValueRecoveredIndividuals,
-                null, // GTA.BasicReproductionNumber,
-                null, // GTA.ControlReproductionNumber,
-                HK.InitialValueSusceptibleIndividuals,
-                HK.BasicTransmissionCoefficient,
-                HK.QuarantineRateAsymptomaticIndividuals,
-                HK.IsolationRateSymptomaticIndividuals,
-                HK.TransmissionCoefficientModificationFactorAsymptomaticIndividuals,
-                HK.TransmissionCoefficientModificationFactorIsolatedIndividuals,
-                HK.TransmissionCoefficientModificationFactorQuarantinedIndividuals,
-                HK.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals,
-                HK.DevelopmentClinicalSymptomsRateQuarantinedIndividuals,
-                HK.NaturalDeathRate,
-                HK.NetInflowRateSusceptibleIndividuals,
-                HK.RecoveryRateSymptomaticIndividuals,
-                HK.RecoveryRateIsolatedIndividuals);
-
-            C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
-                context);
-
-            export.Solve();
-
-            ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> dayCumulativeDiseaseInducedDeaths = export.DayCumulativeDiseaseInducedDeaths;
-
-            ViewData["w1"] = dayCumulativeDiseaseInducedDeaths.Select(w => w.Item1.ToPartialDateTime().Value.ToUniversalTime().DateTime.Date.ToString("MMM dd yyyy")).ToArray();
-
-            ViewData["w2"] = dayCumulativeDiseaseInducedDeaths.Select(w => w.Item2.Value.Value).ToArray();
-
             return View();
         }
 
