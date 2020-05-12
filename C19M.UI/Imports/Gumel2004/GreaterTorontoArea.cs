4519ce4df74a1f16de298038b067cccff0ada13d
Justin Britt <britt@uwindsor.ca>
2020-05-12 16:50:13 -0400

    Added class

---
+++ C19M.UI/Imports/Gumel2004/GreaterTorontoArea.cs
@@ -0,0 +1,15 @@
+﻿namespace C19M.UI.Imports.Gumel2004
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public sealed class GreaterTorontoArea
+    {
+        // Constructor
+        public GreaterTorontoArea()
+        {
+        }
+    }
+}
