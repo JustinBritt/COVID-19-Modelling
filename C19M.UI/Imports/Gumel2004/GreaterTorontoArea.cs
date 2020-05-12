39e74f4c6f2ee981cb20ca13913fe8ff33750aa3
Justin Britt <britt@uwindsor.ca>
2020-05-12 16:52:18 -0400

    Moved code

--- C19M.UI/Imports/Gumel2004/GreaterTorontoArea.cs
+++ C19M.UI/Imports/Gumel2004/GreaterTorontoArea.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Immutable;
     using System.Linq;
     using System.Web;
 
@@ -10,6 +11,70 @@
         // Constructor
         public GreaterTorontoArea()
         {
+            C19M.D.Gumel2004.Interfaces.IGreaterTorontoArea GreaterTorontoArea = new C19M.D.Gumel2004.Classes.GreaterTorontoArea();
+
+            C19M.M.C.A.Gumel2004.Interfaces.Contexts.IGumel2004_Context GreaterTorontoArea_Context = new C19M.M.C.A.Gumel2004.Classes.Contexts.Gumel2004_Context(
+                GreaterTorontoArea.EndDate,
+                GreaterTorontoArea.NumberDaysAfterStartDate,
+                GreaterTorontoArea.StartDate,
+                GreaterTorontoArea.DiseaseInducedDeathRateSymptomaticIndividuals,
+                GreaterTorontoArea.DiseaseInducedDeathRateIsolatedIndividuals,
+                GreaterTorontoArea.InitialValueAsymptomaticIndividuals,
+                GreaterTorontoArea.InitialValueSymptomaticIndividuals,
+                GreaterTorontoArea.InitialValueIsolatedIndividuals,
+                GreaterTorontoArea.RecruitmentRateAsymptomaticIndividuals,
+                GreaterTorontoArea.InitialValueQuarantinedIndividuals,
+                GreaterTorontoArea.InitialValueRecoveredIndividuals,
+                null, // GTA.BasicReproductionNumber,
+                null, // GTA.ControlReproductionNumber,
+                GreaterTorontoArea.InitialValueSusceptibleIndividuals,
+                GreaterTorontoArea.BasicTransmissionCoefficient,
+                GreaterTorontoArea.QuarantineRateAsymptomaticIndividuals,
+                GreaterTorontoArea.IsolationRateSymptomaticIndividuals,
+                GreaterTorontoArea.TransmissionCoefficientModificationFactorAsymptomaticIndividuals,
+                GreaterTorontoArea.TransmissionCoefficientModificationFactorIsolatedIndividuals,
+                GreaterTorontoArea.TransmissionCoefficientModificationFactorQuarantinedIndividuals,
+                GreaterTorontoArea.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals,
+                GreaterTorontoArea.DevelopmentClinicalSymptomsRateQuarantinedIndividuals,
+                GreaterTorontoArea.NaturalDeathRate,
+                GreaterTorontoArea.NetInflowRateSusceptibleIndividuals,
+                GreaterTorontoArea.RecoveryRateSymptomaticIndividuals,
+                GreaterTorontoArea.RecoveryRateIsolatedIndividuals);
+
+            C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export GreaterTorontoArea_Export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
+                GreaterTorontoArea_Context);
+
+            GreaterTorontoArea_Export.Solve();
+
+            this.GreaterTorontoArea_DayCumulativeDiseaseInducedDeaths = GreaterTorontoArea_Export.DayCumulativeDiseaseInducedDeaths;
+
+            this.GreaterTorontoArea_DayAsymptomaticIndividuals = GreaterTorontoArea_Export.DayAsymptomaticIndividuals;
+
+            this.GreaterTorontoArea_DaySymptomaticIndividuals = GreaterTorontoArea_Export.DaySymptomaticIndividuals;
+
+            this.GreaterTorontoArea_DayIsolatedIndividuals = GreaterTorontoArea_Export.DayIsolatedIndividuals;
+
+            this.GreaterTorontoArea_DayQuarantinedIndividuals = GreaterTorontoArea_Export.DayQuarantinedIndividuals;
+
+            this.GreaterTorontoArea_DayRecoveredIndividuals = GreaterTorontoArea_Export.DayRecoveredIndividuals;
+
+            this.GreaterTorontoArea_DaySusceptibleIndividuals = GreaterTorontoArea_Export.DaySusceptibleIndividuals;
         }
+
+        private C19M.D.Gumel2004.Interfaces.IGreaterTorontoArea Data => new C19M.D.Gumel2004.Classes.GreaterTorontoArea();
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DayCumulativeDiseaseInducedDeaths { get; set; }
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DayAsymptomaticIndividuals { get; set; }
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DaySymptomaticIndividuals { get; set; }
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DayIsolatedIndividuals { get; set; }
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DayQuarantinedIndividuals { get; set; }
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DayRecoveredIndividuals { get; set; }
+
+        public ImmutableList<System.Tuple<FhirDateTime, FhirDecimal>> GreaterTorontoArea_DaySusceptibleIndividuals { get; set; }
     }
 }
--- C19M.UI/Models/Gumel2004.cs
+++ C19M.UI/Models/Gumel2004.cs
@@ -14,41 +14,13 @@
         // Constructor
         public Gumel2004()
         {
-            C19M.D.Gumel2004.Interfaces.IGreaterTorontoArea GreaterTorontoArea = new C19M.D.Gumel2004.Classes.GreaterTorontoArea();
+            
 
             C19M.D.Gumel2004.Interfaces.IHongKong HongKong = new C19M.D.Gumel2004.Classes.HongKong();
 
             C19M.D.Gumel2004.Interfaces.ISingapore Singapore = new C19M.D.Gumel2004.Classes.Singapore();
 
             // Contexts
-            C19M.M.C.A.Gumel2004.Interfaces.Contexts.IGumel2004_Context GreaterTorontoArea_Context = new C19M.M.C.A.Gumel2004.Classes.Contexts.Gumel2004_Context(
-                GreaterTorontoArea.EndDate,
-                GreaterTorontoArea.NumberDaysAfterStartDate,
-                GreaterTorontoArea.StartDate,
-                GreaterTorontoArea.DiseaseInducedDeathRateSymptomaticIndividuals,
-                GreaterTorontoArea.DiseaseInducedDeathRateIsolatedIndividuals,
-                GreaterTorontoArea.InitialValueAsymptomaticIndividuals,
-                GreaterTorontoArea.InitialValueSymptomaticIndividuals,
-                GreaterTorontoArea.InitialValueIsolatedIndividuals,
-                GreaterTorontoArea.RecruitmentRateAsymptomaticIndividuals,
-                GreaterTorontoArea.InitialValueQuarantinedIndividuals,
-                GreaterTorontoArea.InitialValueRecoveredIndividuals,
-                null, // GTA.BasicReproductionNumber,
-                null, // GTA.ControlReproductionNumber,
-                GreaterTorontoArea.InitialValueSusceptibleIndividuals,
-                GreaterTorontoArea.BasicTransmissionCoefficient,
-                GreaterTorontoArea.QuarantineRateAsymptomaticIndividuals,
-                GreaterTorontoArea.IsolationRateSymptomaticIndividuals,
-                GreaterTorontoArea.TransmissionCoefficientModificationFactorAsymptomaticIndividuals,
-                GreaterTorontoArea.TransmissionCoefficientModificationFactorIsolatedIndividuals,
-                GreaterTorontoArea.TransmissionCoefficientModificationFactorQuarantinedIndividuals,
-                GreaterTorontoArea.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals,
-                GreaterTorontoArea.DevelopmentClinicalSymptomsRateQuarantinedIndividuals,
-                GreaterTorontoArea.NaturalDeathRate,
-                GreaterTorontoArea.NetInflowRateSusceptibleIndividuals,
-                GreaterTorontoArea.RecoveryRateSymptomaticIndividuals,
-                GreaterTorontoArea.RecoveryRateIsolatedIndividuals);
-
             C19M.M.C.A.Gumel2004.Interfaces.Contexts.IGumel2004_Context HongKong_Context = new C19M.M.C.A.Gumel2004.Classes.Contexts.Gumel2004_Context(
                 HongKong.EndDate,
                 HongKong.NumberDaysAfterStartDate,
@@ -106,34 +78,17 @@
                 Singapore.RecoveryRateIsolatedIndividuals);
 
             // Exports
-            C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export GreaterTorontoArea_Export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
-                GreaterTorontoArea_Context);
-
             C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export HongKong_Export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
                 HongKong_Context);
 
             C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export Singapore_Export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
                 Singapore_Context);
 
-            GreaterTorontoArea_Export.Solve();
-
             HongKong_Export.Solve();
 
             Singapore_Export.Solve();
 
-            this.GreaterTorontoArea_DayCumulativeDiseaseInducedDeaths = GreaterTorontoArea_Export.DayCumulativeDiseaseInducedDeaths;
-
-            this.GreaterTorontoArea_DayAsymptomaticIndividuals = GreaterTorontoArea_Export.DayAsymptomaticIndividuals;
-
-            this.GreaterTorontoArea_DaySymptomaticIndividuals = GreaterTorontoArea_Export.DaySymptomaticIndividuals;
-
-            this.GreaterTorontoArea_DayIsolatedIndividuals = GreaterTorontoArea_Export.DayIsolatedIndividuals;
-
-            this.GreaterTorontoArea_DayQuarantinedIndividuals = GreaterTorontoArea_Export.DayQuarantinedIndividuals;
-
-            this.GreaterTorontoArea_DayRecoveredIndividuals = GreaterTorontoArea_Export.DayRecoveredIndividuals;
-
-            this.GreaterTorontoArea_DaySusceptibleIndividuals = GreaterTorontoArea_Export.DaySusceptibleIndividuals;
+            
 
             this.HongKong_DayCumulativeDiseaseInducedDeaths = HongKong_Export.DayCumulativeDiseaseInducedDeaths;
 
