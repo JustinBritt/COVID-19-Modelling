fbe8e74622aac3ff3834e8bd44dc9de5413052e3
Justin Britt <britt@uwindsor.ca>
2020-05-12 17:48:10 -0400

    Added  class

---
+++ C19M.UI/Models/Gumel2004/Singapore.cs
@@ -0,0 +1,11 @@
+﻿namespace C19M.UI.Models.Gumel2004
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    public sealed class Singapore
+    {
+    }
+}
