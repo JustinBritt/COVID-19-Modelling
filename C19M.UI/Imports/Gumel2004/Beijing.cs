7b168a71a78819ea0fe629bb8efc72b605f45b15
Justin Britt <britt@uwindsor.ca>
2020-05-12 16:05:26 -0400

    Moved code

--- C19M.UI/Imports/Gumel2004/Beijing.cs
+++ C19M.UI/Imports/Gumel2004/Beijing.cs
@@ -16,6 +16,42 @@
         // Constructor
         public Beijing()
         {
+            C19M.D.Gumel2004.Interfaces.IBeijing Beijing = new C19M.D.Gumel2004.Classes.Beijing();
+
+            // Contexts
+            C19M.M.C.A.Gumel2004.Interfaces.Contexts.IGumel2004_Context Beijing_Context = new C19M.M.C.A.Gumel2004.Classes.Contexts.Gumel2004_Context(
+                Beijing.EndDate,
+                Beijing.NumberDaysAfterStartDate,
+                Beijing.StartDate,
+                Beijing.DiseaseInducedDeathRateSymptomaticIndividuals,
+                Beijing.DiseaseInducedDeathRateIsolatedIndividuals,
+                Beijing.InitialValueAsymptomaticIndividuals,
+                Beijing.InitialValueSymptomaticIndividuals,
+                Beijing.InitialValueIsolatedIndividuals,
+                Beijing.RecruitmentRateAsymptomaticIndividuals,
+                Beijing.InitialValueQuarantinedIndividuals,
+                Beijing.InitialValueRecoveredIndividuals,
+                null, // GTA.BasicReproductionNumber,
+                null, // GTA.ControlReproductionNumber,
+                Beijing.InitialValueSusceptibleIndividuals,
+                Beijing.BasicTransmissionCoefficient,
+                Beijing.QuarantineRateAsymptomaticIndividuals,
+                Beijing.IsolationRateSymptomaticIndividuals,
+                Beijing.TransmissionCoefficientModificationFactorAsymptomaticIndividuals,
+                Beijing.TransmissionCoefficientModificationFactorIsolatedIndividuals,
+                Beijing.TransmissionCoefficientModificationFactorQuarantinedIndividuals,
+                Beijing.DevelopmentClinicalSymptomsRateAsymptomaticIndividuals,
+                Beijing.DevelopmentClinicalSymptomsRateQuarantinedIndividuals,
+                Beijing.NaturalDeathRate,
+                Beijing.NetInflowRateSusceptibleIndividuals,
+                Beijing.RecoveryRateSymptomaticIndividuals,
+                Beijing.RecoveryRateIsolatedIndividuals);
+
+            // Exports
+            C19M.M.C.A.Gumel2004.Interfaces.Exports.IGumel2004_Export Beijing_Export = new C19M.M.C.A.Gumel2004.Classes.Exports.Gumel2004_Export(
+                Beijing_Context);
+
+            Beijing_Export.Solve();
         }
 
         private C19M.D.Gumel2004.Interfaces.IBeijing Data => new C19M.D.Gumel2004.Classes.Beijing();
