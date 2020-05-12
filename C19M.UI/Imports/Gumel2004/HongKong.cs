f5cd27bf0c5589ed415490844df02930ad9f96fe
Justin Britt <britt@uwindsor.ca>
2020-05-12 17:17:19 -0400

    Added class

---
+++ C19M.UI/Imports/Gumel2004/HongKong.cs
@@ -0,0 +1,19 @@
+﻿namespace C19M.UI.Imports.Gumel2004
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Web;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    public sealed class HongKong
+    {
+        // Constructor
+        public HongKong()
+        {
+        }
+    }
+}
