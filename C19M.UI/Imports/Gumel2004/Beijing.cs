5ee149b7e929d47e69ecd336bdd3ba45ff95de43
Justin Britt <britt@uwindsor.ca>
2020-05-12 15:59:49 -0400

    Changes to usings

--- C19M.UI/Imports/Gumel2004/Beijing.cs
+++ C19M.UI/Imports/Gumel2004/Beijing.cs
@@ -2,8 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Immutable;
     using System.Linq;
     using System.Web;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    using C19M.D.Gumel2004.Interfaces;
 
     public sealed class Beijing
     {
