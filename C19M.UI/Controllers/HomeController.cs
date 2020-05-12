aa19817c52f7829b1e8f566472b0fbc330b6c2ce
Justin Britt <britt@uwindsor.ca>
2020-05-12 17:42:38 -0400

    Formatting changes

--- C19M.UI/Controllers/HomeController.cs
+++ C19M.UI/Controllers/HomeController.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Web;
-using System.Web.Helpers;
-using System.Web.Mvc;
-
-using Hl7.Fhir.Model;
-
-using C19M.M.C.A.Gumel2004.Interfaces.Contexts;
-
-using C19M.M.C.A.Gumel2004.Interfaces.Exports;
-using C19M.UI.Models;
-
-namespace C19M.UI.Controllers
+﻿namespace C19M.UI.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Helpers;
+    using System.Web.Mvc;
+
+    using Hl7.Fhir.Model;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Contexts;
+
+    using C19M.M.C.A.Gumel2004.Interfaces.Exports;
+    using C19M.UI.Models;
+
     public class HomeController : Controller
     {
         public ActionResult Index()
