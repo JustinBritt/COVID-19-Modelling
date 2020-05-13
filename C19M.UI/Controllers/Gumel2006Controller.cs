0f16700dee96cce9b96d90c1bde300154a636709
Justin Britt <britt@uwindsor.ca>
2020-05-13 01:00:12 -0400

    Added Controller

---
+++ C19M.UI/Controllers/Gumel2006Controller.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.UI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public sealed class Gumel2006Controller : Controller
+    {
+        // GET: Gumel2006
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
