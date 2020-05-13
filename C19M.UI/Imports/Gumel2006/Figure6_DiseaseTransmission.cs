7499f1fd4e2332a398128919466f4620ca105459
Justin Britt <britt@uwindsor.ca>
2020-05-13 01:47:23 -0400

    Added class

---
+++ C19M.UI/Imports/Gumel2006/Figure6_DiseaseTransmission.cs
@@ -0,0 +1,18 @@
+﻿namespace C19M.UI.Imports.Gumel2006
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    public sealed class Figure6_DiseaseTransmission
+    {
+        // Constructor
+        public Figure6_DiseaseTransmission()
+        {
+        }
+    }
+}
