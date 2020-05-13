05ac982e4d1a90ded2b62d04fa266d70ad1e44a4
Justin Britt <britt@uwindsor.ca>
2020-05-13 01:13:01 -0400

    Added class

---
+++ C19M.D.Gumel2006/Interfaces/IFigure3.cs
@@ -0,0 +1,75 @@
+﻿namespace C19M.D.Gumel2006.Interfaces
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    // Hl7
+    using Hl7.Fhir.Model;
+
+    public interface IFigure3
+    {
+        /// <summary>
+        /// Gets the disease-induced mortality rate.
+        /// Parameter: d
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal DiseaseInducedMortalityRate { get; }
+
+        /// <summary>
+        /// Gets the equilibrium population (without disease).
+        /// Parameter: N_o
+        /// </summary>
+        FhirDecimal EquilibriumPopulationWithoutDisease { get; }
+
+        /// <summary>
+        /// Gets the rate of development of clinical symptoms.
+        /// Parameter: α
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal DevelopmentClinicalSymptomsRate { get; }
+
+        /// <summary>
+        /// Gets the effective contact rate.
+        /// Parameter: β
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal EffectiveContactRate { get; }
+
+        /// <summary>
+        /// Gets the recovery rate.
+        /// Parameter: δ
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal RecoveryRate { get; }
+
+        /// <summary>
+        /// Gets the natural mortality rate.
+        /// Parameter: μ
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal NaturalMortalityRate { get; }
+
+        /// <summary>
+        /// Gets the vaccination coverage rate.
+        /// Parameter: ξ
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal VaccinationCoverageRate { get; }
+
+        /// <summary>
+        /// Gets the recruitment rate of susceptible humans.
+        /// Parameter: Π
+        /// Units: Per day
+        /// </summary>
+        FhirDecimal RecruitmentRateSusceptibleHumans { get; }
+
+        /// <summary>
+        /// Gets the efficacy of the vaccine.
+        /// Parameter: τ
+        /// </summary>
+        FhirDecimal VaccineEfficacy { get; }
+    }
+}
