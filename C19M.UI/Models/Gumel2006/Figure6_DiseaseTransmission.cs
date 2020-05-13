9749ad9f703484af5bba3fbd207e389ad9c518d0
Justin Britt <britt@uwindsor.ca>
2020-05-13 02:22:02 -0400

    Added properties

--- C19M.UI/Models/Gumel2006/Figure6_DiseaseTransmission.cs
+++ C19M.UI/Models/Gumel2006/Figure6_DiseaseTransmission.cs
@@ -15,5 +15,23 @@
         }
 
         public C19M.UI.Imports.Gumel2006.Figure6_DiseaseTransmission Import { get; set; }
+
+        public decimal[] DayCumulativeDiseaseInducedDeaths => this.Import.DayCumulativeDiseaseInducedDeaths.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayCumulativeProbableCases => this.Import.DayCumulativeProbableCases.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayDiseaseInducedDeaths => this.Import.DayDiseaseInducedDeaths.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayProbableCases => this.Import.DayProbableCases.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayLatentIndividuals => this.Import.DayLatentIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayInfectedIndividuals => this.Import.DayInfectedIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayTreatedIndividuals => this.Import.DayTreatedIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DaySusceptibleIndividuals => this.Import.DaySusceptibleIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayVaccinatedIndividuals => this.Import.DayVaccinatedIndividuals.Select(w => w.Item2.Value.Value).ToArray();
     }
 }
