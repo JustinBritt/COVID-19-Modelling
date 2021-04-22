3499b96638afaac32ffcc5af776ade44900a7bb3
Justin Britt <britt@uwindsor.ca>
2021-04-22 08:46:15 -0400

    Added class

---
+++ C19M.UI/Models/Gumel2004/GreaterTorontoAreaModel.cs
@@ -0,0 +1,38 @@
+﻿namespace C19M.UI.Models.Gumel2004
+{
+    using System;
+    using System.Linq;
+
+    using Hl7.Fhir.Model;
+
+    using C19M.UI.Imports.Gumel2004;
+
+    public sealed class GreaterTorontoAreaModel
+    {
+        // Constructor
+        public GreaterTorontoAreaModel()
+        {
+            // Imports
+            this.Import = new C19M.UI.Imports.Gumel2004.GreaterTorontoArea();
+        }
+
+        public C19M.UI.Imports.Gumel2004.GreaterTorontoArea Import { get; set; }
+
+        // TODO: Update
+        public string[] Days => this.Import.DayCumulativeDiseaseInducedDeaths.Select(w => w.Item1.ToDateTimeOffset(TimeSpan.Zero).UtcDateTime.Date.ToString("MMM dd yyyy")).ToArray();
+
+        public decimal[] DayCumulativeDiseaseInducedDeaths_Array => this.Import.DayCumulativeDiseaseInducedDeaths.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayAsymptomaticIndividuals_Array => this.Import.DayAsymptomaticIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DaySymptomaticIndividuals_Array => this.Import.DaySymptomaticIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayIsolatedIndividuals_Array => this.Import.DayIsolatedIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayQuarantinedIndividuals_Array => this.Import.DayQuarantinedIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DayRecoveredIndividuals_Array => this.Import.DayRecoveredIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+
+        public decimal[] DaySusceptibleIndividuals_Array => this.Import.DaySusceptibleIndividuals.Select(w => w.Item2.Value.Value).ToArray();
+    }
+}
