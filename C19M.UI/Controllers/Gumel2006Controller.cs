ea739bd003368a79fd5d8262f004f26548516f0f
Justin Britt <britt@uwindsor.ca>
2021-04-22 08:59:23 -0400

    Added class

---
+++ C19M.UI/Controllers/Gumel2006Controller.cs
@@ -0,0 +1,30 @@
+﻿namespace C19M.UI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public sealed class Gumel2006Controller : Controller
+    {
+        // GET: Gumel2006
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Gumel2006
+        public IActionResult Gumel2006()
+        {
+            return PartialView();
+        }
+
+        public IActionResult Figure6()
+        {
+            return PartialView(
+                new C19M.UI.Models.Gumel2006.Figure6Model());
+        }
+    }
+}
