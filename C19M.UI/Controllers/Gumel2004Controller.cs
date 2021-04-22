f14f6a36f88bedd52723418f6f030e31c2745e21
Justin Britt <britt@uwindsor.ca>
2021-04-22 08:55:02 -0400

    Added class

---
+++ C19M.UI/Controllers/Gumel2004Controller.cs
@@ -0,0 +1,17 @@
+﻿namespace C19M.UI.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    public sealed class Gumel2004Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
