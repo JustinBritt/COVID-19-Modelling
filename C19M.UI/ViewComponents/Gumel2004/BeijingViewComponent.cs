b539ccdfad63b049785e45820a435280c89863e7
Justin Britt <britt@uwindsor.ca>
2021-04-23 17:53:14 -0400

    Added View and ViewComponent

---
+++ C19M.UI/ViewComponents/Gumel2004/BeijingViewComponent.cs
@@ -0,0 +1,23 @@
+﻿namespace ViewComponentSample.ViewComponents.Gumel2004
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class BeijingViewComponent : ViewComponent
+    {
+        public BeijingViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string MyView = "Beijing2004";
+
+            return View(
+                MyView);
+        }
+    }
+}
