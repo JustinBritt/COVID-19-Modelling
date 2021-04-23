ef1757a7d6cbcdff3872289392c72a36d68378f0
Justin Britt <britt@uwindsor.ca>
2021-04-23 17:39:47 -0400

    Added ViewComponent

---
+++ C19M.UI/ViewComponents/Gumel2004ViewComponent.cs
@@ -0,0 +1,20 @@
+﻿namespace ViewComponentSample.ViewComponents
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class Gumel2004ViewComponent : ViewComponent
+    {
+        public Gumel2004ViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
