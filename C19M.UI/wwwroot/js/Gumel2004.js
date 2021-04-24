106bc886d40b80c13dc3ca3f1a32acbc49ccfef8
Justin Britt <britt@uwindsor.ca>
2021-04-24 12:15:20 -0400

    Formatting changes

--- C19M.UI/wwwroot/js/Gumel2004.js
+++ C19M.UI/wwwroot/js/Gumel2004.js
@@ -19,12 +19,14 @@
 }
 
 function GenerateLabelsGumel2004(
-    days) {
+    days)
+{
     let data = [];
 
     let i = 0;
 
-    for (; i < days.length; i++) {
+    for (; i < days.length; i++)
+    {
         data.push(
             {
                 x: moment(days[i])
@@ -48,7 +50,8 @@ function GenerateChartGumel(
     daySusceptibleIndividuals,
     configuration_options_scales_x_scaleLabel_labelString,
     configuration_options_scales_y_scaleLabel_labelString,
-    configuration_options_title_text) {
+    configuration_options_title_text)
+{
 
     const context = document.getElementById(canvas).getContext('2d');
     context.canvas.width = context_canvas_width;
@@ -56,7 +59,8 @@ function GenerateChartGumel(
 
     const color = Chart.helpers.color;
 
-    let dayCumulativeDiseaseInducedDeathsDataset = {
+    let dayCumulativeDiseaseInducedDeathsDataset =
+    {
         label: 'Beijing',
         backgroundColor: color(window.chartColors.orange).alpha(0.5).rgbString(),
         borderColor: window.chartColors.orange,
@@ -70,7 +74,8 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    let dayAsymptomaticIndividualsDataset = {
+    let dayAsymptomaticIndividualsDataset =
+    {
         label: 'Asymptomatic Individuals',
         backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
         borderColor: window.chartColors.red,
@@ -84,7 +89,8 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    let daySymptomaticIndividualsDataset = {
+    let daySymptomaticIndividualsDataset =
+    {
         label: 'Symptomatic Individuals',
         backgroundColor: color(window.chartColors.orange).alpha(0.5).rgbString(),
         borderColor: window.chartColors.orange,
@@ -98,7 +104,8 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    let dayIsolatedIndividualsDataset = {
+    let dayIsolatedIndividualsDataset =
+    {
         label: 'Isolated Individuals',
         backgroundColor: color(window.chartColors.yellow).alpha(0.5).rgbString(),
         borderColor: window.chartColors.yellow,
@@ -112,7 +119,8 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    let dayQuarantinedIndividualsDataset = {
+    let dayQuarantinedIndividualsDataset =
+    {
         label: 'Quarantined Individuals',
         backgroundColor: color(window.chartColors.green).alpha(0.5).rgbString(),
         borderColor: window.chartColors.green,
@@ -126,7 +134,8 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    let dayRecoveredIndividualsDataset = {
+    let dayRecoveredIndividualsDataset =
+    {
         label: 'Recovered Individuals',
         backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
         borderColor: window.chartColors.blue,
@@ -140,7 +149,8 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    let daySusceptibleIndividualsDataset = {
+    let daySusceptibleIndividualsDataset =
+    {
         label: 'Susceptible Individuals',
         backgroundColor: color(window.chartColors.purple).alpha(0.5).rgbString(),
         borderColor: window.chartColors.purple,
@@ -154,9 +164,11 @@ function GenerateChartGumel(
         borderWidth: 2
     };
 
-    const configuration = {
+    const configuration =
+    {
         type: 'line',
-        data: {
+        data:
+        {
             labels: GenerateLabelsGumel2004(
                 days),
             datasets: [
@@ -167,19 +179,25 @@ function GenerateChartGumel(
                 dayRecoveredIndividualsDataset,
                 daySusceptibleIndividualsDataset]
         },
-        options: {
-            scales: {
-                x: {
+        options:
+        {
+            scales:
+            {
+                x:
+                {
                     max: moment(days[days.length - 1]).valueOf(),
                     min: moment(days[0]).valueOf(),
                     type: 'timeseries',
-                    title: {
+                    title:
+                    {
                         display: true,
                         text: configuration_options_scales_x_scaleLabel_labelString
                     },
                     offset: true,
-                    ticks: {
-                        major: {
+                    ticks:
+                    {
+                        major:
+                        {
                             enabled: true,
                         },
                         fontStyle: 'bold',
@@ -190,32 +208,42 @@ function GenerateChartGumel(
                         sampleSize: 200
                     },
                 },
-                y: {
+                y:
+                {
                     type: 'logarithmic',
-                    gridLines: {
+                    gridLines:
+                    {
                         drawBorder: false
                     },
-                    title: {
+                    title:
+                    {
                         display: true,
                         text: configuration_options_scales_y_scaleLabel_labelString
                     }
                 }
             },
-            title: {
+            title:
+            {
                 display: true,
                 text: configuration_options_title_text
             },
-            plugins: {
-                tooltip: {
-                    callbacks: {
-                        label: function (context) {
+            plugins:
+            {
+                tooltip:
+                {
+                    callbacks:
+                    {
+                        label: function (context)
+                        {
                             var label = context.dataset.label || '';
 
-                            if (label) {
+                            if (label)
+                            {
                                 label += ': ';
                             }
 
-                            if (context.parsed.y !== null) {
+                            if (context.parsed.y !== null)
+                            {
                                 label += parseFloat(context.parsed.y).toFixed(2);
                             }
 
